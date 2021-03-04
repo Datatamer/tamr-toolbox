6f98e0df042c1f7162165a45e9b8589c8ed478af
nguyentr17 <nguyentr17@grinnell.edu>
2021-03-04 17:16:47 -0500

    modify tests to change subset to exact match and remove online test global variable

--- tests/utils/test_downstream.py
+++ tests/utils/test_downstream.py
@@ -8,10 +8,9 @@ CONFIG = tamr_toolbox.utils.config.from_yaml(
 )
 MASTERING_UNIFIED_DATASET_ID = CONFIG["datasets"]["minimal_mastering_unified_dataset"]
 SOURCE_DATASET_ID = CONFIG["datasets"]["people_tiny.csv"]
-ONLINE_TEST = False
 
 
-@mock_api(enforce_online_test=ONLINE_TEST)
+@mock_api()
 def test_get_dataset_downstream_dependencies():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     dataset = client.datasets.by_resource_id(MASTERING_UNIFIED_DATASET_ID)
@@ -59,10 +58,10 @@ def test_get_dataset_downstream_dependencies():
         "minimal_mastering_unified_dataset_dedup_clusters_with_data",
         "minimal_mastering_unified_dataset_dedup_signals",
     }
-    assert PIPELINE_DEPENDENT_DATASETS.issubset(set([d.name for d in downstream_datasets]))
+    assert PIPELINE_DEPENDENT_DATASETS == set([d.name for d in downstream_datasets])
 
 
-@mock_api(enforce_online_test=ONLINE_TEST)
+@mock_api()
 def test_get_dataset_downstream_dependencies_suggest_name():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     dataset = client.datasets.by_resource_id(MASTERING_UNIFIED_DATASET_ID)
@@ -72,7 +71,49 @@ def test_get_dataset_downstream_dependencies_suggest_name():
     # check returned results
     # in newer version of Tamr, this number might increase
     # because more persistence data is moved to HBase
-    assert len(downstream_datasets) >= 49
+    assert len(downstream_datasets) == 49
+    PIPELINE_DEPENDENT_DATASETS = {
+        "minimal_mastering_unified_dataset_dedup_user_defined_signals",
+        "minimal_golden_records_source_list_diff",
+        "minimal_mastering_unified_dataset_dedup_clusters_union",
+        "minimal_mastering_unified_dataset_dedup_dnf_binning",
+        "minimal_mastering_unified_dataset_dedup_cluster_stats_union",
+        "minimal_golden_records_golden_records_cluster_profile",
+        "minimal_mastering_unified_dataset_dedup_published_cluster_stats",
+        "minimal_mastering_unified_dataset_dedup_pair_comments",
+        "minimal_mastering_unified_dataset_dedup_function_predictions",
+        "minimal_mastering_unified_dataset_important_pairs",
+        "minimal_mastering_unified_dataset_dedup_features",
+        "minimal_golden_records_golden_records",
+        "minimal_mastering_unified_dataset_dedup_clusters_with_stats_union",
+        "minimal_mastering_unified_dataset_dedup_cluster_accuracy_metrics",
+        "minimal_mastering_unified_dataset_dedup_idf",
+        "minimal_mastering_unified_dataset_dedup_labels",
+        "minimal_mastering_unified_dataset_dedup_clusters",
+        "minimal_mastering_unified_dataset_dedup_published_clusters_with_data",
+        "minimal_mastering_unified_dataset_dedup_test_records",
+        "minimal_mastering_unified_dataset_dedup_training_clusters",
+        "minimal_mastering_unified_dataset_dedup_published_clusters",
+        "minimal_mastering_unified_dataset_dedup_test_clusters",
+        "minimal_mastering_unified_dataset_dedup_feedback",
+        "minimal_golden_records_golden_records_draft",
+        "minimal_mastering_unified_dataset_dedup_high_impact_questions",
+        "minimal_mastering_unified_dataset_dedup_test_records_accuracy",
+        "minimal_mastering_unified_dataset_dedup_model",
+        "minimal_golden_records_source_list",
+        "minimal_mastering_unified_dataset_dedup_all_persistent_ids",
+        "minimal_mastering_unified_dataset_dedup_human_signals",
+        "minimal_mastering_unified_dataset_dedup_cluster_similarities",
+        "minimal_mastering_unified_dataset_dedup_non_null_count",
+        "minimal_mastering_unified_dataset_dedup_dnf_signals",
+        "minimal_mastering_unified_dataset_dedup_published_cluster_counts",
+        "minimal_golden_records_golden_records_pinned_cluster_input",
+        "minimal_mastering_unified_dataset_dedup_cluster_stats",
+        "minimal_mastering_unified_dataset_dedup_signals_predictions",
+        "minimal_golden_records_golden_records_rule_output",
+        "minimal_mastering_unified_dataset_dedup_clusters_with_data",
+        "minimal_mastering_unified_dataset_dedup_signals",
+    }
     DEPENDENT_DATASETS_BY_NAME = {
         "minimal_mastering_unified_dataset_dedup_verified_cluster_members",
         "minimal_mastering_unified_dataset.recordPairLabel",
@@ -84,10 +125,10 @@ def test_get_dataset_downstream_dependencies_suggest_name():
         "minimal_mastering_unified_dataset.internal_comments",
         "minimal_mastering_unified_dataset.global_record_id",
     }
-    assert DEPENDENT_DATASETS_BY_NAME.issubset(set([d.name for d in downstream_datasets]))
+    assert PIPELINE_DEPENDENT_DATASETS.union(DEPENDENT_DATASETS_BY_NAME) == set([d.name for d in downstream_datasets])
 
 
-@mock_api(enforce_online_test=ONLINE_TEST)
+@mock_api()
 def test_get_downstream_projects():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     dataset = client.datasets.by_resource_id(MASTERING_UNIFIED_DATASET_ID)
@@ -95,16 +136,11 @@ def test_get_downstream_projects():
         dataset, include_dependencies_by_name=True
     )
     assert len(downstream_projects) == 2
-    assert (
-        str(downstream_projects) == "[tamr_unify_client.project.resource.Project(relative_id="
-        "'projects/1', name='minimal_mastering', "
-        "type='DEDUP'), tamr_unify_client.project.resource.Project"
-        "(relative_id='projects/2', name='minimal_golden_records', "
-        "type='GOLDEN_RECORDS')]"
-    )
+    assert downstream_projects[0].relative_id == 'projects/1'
+    assert downstream_projects[1].relative_id == 'projects/2'
 
 
-@mock_api(enforce_online_test=ONLINE_TEST)
+@mock_api()
 def test_get_downstream_datasets_for_source_dataset():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     source_dataset = client.datasets.by_resource_id(SOURCE_DATASET_ID)
@@ -117,7 +153,7 @@ def test_get_downstream_datasets_for_source_dataset():
     assert unified_downstream_datasets_names.issubset(source_downstream_datasets_names)
 
 
-@mock_api(enforce_online_test=ONLINE_TEST)
+@mock_api()
 def test_get_downstream_datasets_for_source_dataset_suggest_name():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     source_dataset = client.datasets.by_resource_id(SOURCE_DATASET_ID)
