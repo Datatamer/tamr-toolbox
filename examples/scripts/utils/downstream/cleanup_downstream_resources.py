f12efd31db07cd6489d6539e2824c9925263373d
nguyentr17 <nguyentr17@grinnell.edu>
2021-03-04 16:49:46 -0500

    refactor to import tamr_toolbox package in example script

--- examples/scripts/utils/downstream/cleanup_downstream_resources.py
+++ examples/scripts/utils/downstream/cleanup_downstream_resources.py
@@ -3,7 +3,7 @@ Example script for checking and cleaning up downstream datasets.
 NOTE: USE AT YOUR OWN RISK. Deleting datasets can sometimes break the system
 and should only be done when necessary. Always take backup before you perform the cleanup.
 """
-from tamr_toolbox import utils
+import tamr_toolbox as tbox
 from requests.exceptions import HTTPError
 from typing import Dict, Any
 import argparse
@@ -11,7 +11,7 @@ import argparse
 
 def main(*, instance_connection_info: Dict[str, Any], dataset_name_to_cleanup: str) -> bool:
     # Create Tamr Client
-    tamr = utils.client.create(**instance_connection_info)
+    tamr = tbox.utils.client.create(**instance_connection_info)
     my_dataset = tamr.datasets.by_name(dataset_name_to_cleanup)
 
     # Get the list of downstream datasets by pipeline dependency
@@ -20,8 +20,8 @@ def main(*, instance_connection_info: Dict[str, Any], dataset_name_to_cleanup: s
     # set `include_dependencies_by_name` to True but check
     # all downstream datasets before deleting to make sure you do not delete
     # any unexpected datasets
-    downstream_datasets = utils.downstream.datasets(my_dataset)
-    downstream_projects = utils.downstream.projects(my_dataset)
+    downstream_datasets = tbox.utils.downstream.datasets(my_dataset)
+    downstream_projects = tbox.utils.downstream.projects(my_dataset)
 
     # Clean up if there is no downstream projects
     if len(downstream_projects) == 0:
@@ -56,11 +56,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Load the configuration from the file path provided or the default file path specified
-    CONFIG = utils.config.from_yaml(
+    CONFIG = tbox.utils.config.from_yaml(
         path_to_file=args.config, default_path_to_file="/path/to/my/conf/project.config.yaml",
     )
     # Use the configuration to create a global logger
-    LOGGER = utils.logger.create(__name__, log_directory=CONFIG["logging_dir"])
+    LOGGER = tbox.utils.logger.create(__name__, log_directory=CONFIG["logging_dir"])
     # Run the main function
     main(
         instance_connection_info=CONFIG["my_tamr_instance"],
