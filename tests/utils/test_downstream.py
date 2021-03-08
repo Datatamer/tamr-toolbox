25be1712859b05c4a732d42469ddea8f04a02b19
nguyentr17 <nguyentr17@grinnell.edu>
2021-03-08 12:05:50 -0500

    change to test equal for downstream datasets of source dataset

--- tests/utils/test_downstream.py
+++ tests/utils/test_downstream.py
@@ -146,27 +146,123 @@ def test_get_downstream_projects():
 def test_get_downstream_datasets_for_source_dataset():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     source_dataset = client.datasets.by_resource_id(SOURCE_DATASET_ID)
-    unified_dataset = client.datasets.by_resource_id(MASTERING_UNIFIED_DATASET_ID)
     source_downstream_datasets = tamr_toolbox.utils.downstream.datasets(source_dataset)
-    unified_downstream_datasets = tamr_toolbox.utils.downstream.datasets(unified_dataset)
-    source_downstream_datasets_names = set([d.name for d in source_downstream_datasets])
-    unified_downstream_datasets_names = set([d.name for d in unified_downstream_datasets])
-    # All downstream datasets of unified dataset must be downstream datasets of the source dataset
-    assert unified_downstream_datasets_names.issubset(source_downstream_datasets_names)
+    source_downstream_datasets_names = {d.name for d in source_downstream_datasets}
+    # List of downstream datasets of a source dataset includes
+    # all derived datasets from projects the source dataset is added as input
+    SOURCE_DATASET_DEPENDENCIES = {
+        "minimal_mastering_unified_dataset_dedup_cluster_similarities",
+        "minimal_mastering_unified_dataset_important_pairs",
+        "minimal_mastering_unified_dataset_dedup_high_impact_questions",
+        "minimal_schema_mapping_unified_dataset",
+        "minimal_mastering_unified_dataset_dedup_pair_comments",
+        "minimal_mastering_unified_dataset_dedup_test_clusters",
+        "minimal_mastering_unified_dataset_dedup_user_defined_signals",
+        "minimal_mastering_unified_dataset_dedup_published_cluster_stats",
+        "minimal_mastering_unified_dataset_dedup_signals_predictions",
+        "minimal_mastering_unified_dataset_dedup_human_signals",
+        "minimal_mastering_unified_dataset",
+        "chained_minimal_schema_mapping_unified_dataset",
+        "minimal_mastering_unified_dataset_dedup_all_persistent_ids",
+        "minimal_mastering_unified_dataset_dedup_cluster_accuracy_metrics",
+        "minimal_golden_records_golden_records_cluster_profile",
+        "minimal_mastering_unified_dataset_dedup_cluster_stats_union",
+        "minimal_mastering_unified_dataset_dedup_clusters_union",
+        "minimal_mastering_unified_dataset_dedup_training_clusters",
+        "minimal_mastering_unified_dataset_dedup_non_null_count",
+        "minimal_mastering_unified_dataset_dedup_test_records",
+        "minimal_mastering_unified_dataset_dedup_labels",
+        "minimal_mastering_unified_dataset_dedup_function_predictions",
+        "minimal_mastering_unified_dataset_dedup_published_cluster_counts",
+        "minimal_mastering_unified_dataset_dedup_clusters_with_stats_union",
+        "people_tiny.csv_sample",
+        "minimal_mastering_unified_dataset_dedup_idf",
+        "minimal_mastering_unified_dataset_dedup_features",
+        "minimal_golden_records_golden_records",
+        "minimal_golden_records_golden_records_rule_output",
+        "minimal_mastering_unified_dataset_dedup_signals",
+        "minimal_mastering_unified_dataset_dedup_clusters",
+        "minimal_mastering_unified_dataset_dedup_published_clusters",
+        "minimal_mastering_unified_dataset_dedup_dnf_binning",
+        "minimal_mastering_unified_dataset_dedup_test_records_accuracy",
+        "minimal_mastering_unified_dataset_dedup_dnf_signals",
+        "minimal_golden_records_golden_records_pinned_cluster_input",
+        "minimal_mastering_unified_dataset_dedup_feedback",
+        "minimal_mastering_unified_dataset_dedup_published_clusters_with_data",
+        "minimal_mastering_unified_dataset_dedup_model",
+        "minimal_golden_records_source_list",
+        "minimal_golden_records_source_list_diff",
+        "minimal_mastering_unified_dataset_dedup_cluster_stats",
+        "minimal_mastering_unified_dataset_dedup_clusters_with_data",
+        "minimal_golden_records_golden_records_draft"
+    }
+    assert source_downstream_datasets_names == SOURCE_DATASET_DEPENDENCIES
 
 
 @mock_api()
 def test_get_downstream_datasets_for_source_dataset_suggest_name():
     client = tamr_toolbox.utils.client.create(**CONFIG["toolbox_test_instance"])
     source_dataset = client.datasets.by_resource_id(SOURCE_DATASET_ID)
-    unified_dataset = client.datasets.by_resource_id(MASTERING_UNIFIED_DATASET_ID)
     source_downstream_datasets = tamr_toolbox.utils.downstream.datasets(
         source_dataset, include_dependencies_by_name=True
     )
-    unified_downstream_datasets = tamr_toolbox.utils.downstream.datasets(
-        unified_dataset, include_dependencies_by_name=True
-    )
-    source_downstream_datasets_names = set([d.name for d in source_downstream_datasets])
-    unified_downstream_datasets_names = set([d.name for d in unified_downstream_datasets])
-    # All downstream datasets of unified dataset must be downstream datasets of the source dataset
-    assert unified_downstream_datasets_names.issubset(source_downstream_datasets_names)
+    source_downstream_datasets_names = {d.name for d in source_downstream_datasets}
+    # When `include_dependencies_by_name=True`, need to return
+    # dependencies suggested by name for unified datasets that are downstream
+    # of a source dataset
+    SOURCE_DATASET_DEPENDENCIES_SUGGEST_NAME = {
+        "minimal_mastering_unified_dataset_dedup_cluster_similarities",
+        "minimal_mastering_unified_dataset_important_pairs",
+        "minimal_mastering_unified_dataset_dedup_verified_cluster_members",
+        "minimal_mastering_unified_dataset_dedup_high_impact_questions",
+        "minimal_schema_mapping_unified_dataset",
+        "minimal_mastering_unified_dataset.recordPairLabel",
+        "minimal_mastering_unified_dataset.internal_links",
+        "minimal_mastering_unified_dataset_dedup_pair_comments",
+        "minimal_mastering_unified_dataset_dedup_test_clusters",
+        "minimal_mastering_unified_dataset_dedup_user_defined_signals",
+        "minimal_mastering_unified_dataset_dedup_published_cluster_stats",
+        "minimal_mastering_unified_dataset_dedup_signals_predictions",
+        "minimal_mastering_unified_dataset_dedup_human_signals",
+        "minimal_mastering_unified_dataset",
+        "chained_minimal_schema_mapping_unified_dataset",
+        "minimal_mastering_unified_dataset_dedup_all_persistent_ids",
+        "minimal_mastering_unified_dataset_dedup_cluster_accuracy_metrics",
+        "minimal_golden_records_golden_records_cluster_profile",
+        "minimal_mastering_unified_dataset.record_pair_id_string",
+        "minimal_mastering_unified_dataset_dedup_cluster_stats_union",
+        "minimal_mastering_unified_dataset_dedup_clusters_union",
+        "minimal_mastering_unified_dataset_dedup_training_clusters",
+        "minimal_mastering_unified_dataset_dedup_non_null_count",
+        "minimal_mastering_unified_dataset_dedup_test_records",
+        "minimal_mastering_unified_dataset_dedup_labels",
+        "minimal_mastering_unified_dataset_dedup_function_predictions",
+        "minimal_mastering_unified_dataset.cluster_feedback",
+        "minimal_mastering_unified_dataset_dedup_published_cluster_counts",
+        "minimal_mastering_unified_dataset_dedup_clusters_with_stats_union",
+        "people_tiny.csv_sample",
+        "minimal_mastering_unified_dataset.internal_comments",
+        "minimal_mastering_unified_dataset_dedup_idf",
+        "minimal_golden_records_golden_records",
+        "minimal_mastering_unified_dataset_dedup_features",
+        "minimal_mastering_unified_dataset.record_pair_feedback_uid",
+        "minimal_golden_records_golden_records_rule_output",
+        "minimal_mastering_unified_dataset_dedup_signals",
+        "minimal_mastering_unified_dataset_dedup_clusters",
+        "minimal_mastering_unified_dataset_dedup_suggested_clusters_log",
+        "minimal_mastering_unified_dataset_dedup_published_clusters",
+        "minimal_mastering_unified_dataset.global_record_id",
+        "minimal_mastering_unified_dataset_dedup_dnf_binning",
+        "minimal_mastering_unified_dataset_dedup_test_records_accuracy",
+        "minimal_mastering_unified_dataset_dedup_dnf_signals",
+        "minimal_golden_records_golden_records_pinned_cluster_input",
+        "minimal_mastering_unified_dataset_dedup_feedback",
+        "minimal_mastering_unified_dataset_dedup_published_clusters_with_data",
+        "minimal_mastering_unified_dataset_dedup_model",
+        "minimal_golden_records_source_list",
+        "minimal_golden_records_source_list_diff",
+        "minimal_mastering_unified_dataset_dedup_cluster_stats",
+        "minimal_mastering_unified_dataset_dedup_clusters_with_data",
+        "minimal_golden_records_golden_records_draft"
+    }
+    assert source_downstream_datasets_names == SOURCE_DATASET_DEPENDENCIES_SUGGEST_NAME
