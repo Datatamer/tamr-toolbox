aaab9a2b3cfc3e88cb7bb5e18d6becf221cdae52
Tamiro Villazon Scholer <tamiro@dhcp-wifi-8021x-155-41-107-224.bu.edu>
2021-04-23 15:49:27 -0400

    adding docstrings and comments

--- tamr_toolbox/project/_common/movement.py
+++ tamr_toolbox/project/_common/movement.py
@@ -1,3 +1,4 @@
+"""Tasks related to project movement as part of Tamr projects"""
 from typing import List, Optional
 from time import sleep
 import logging
@@ -12,20 +13,37 @@ LOGGER = logging.getLogger(__name__)
 
 
 def export_project(
-        tamr_project: Project, *, directory_path: str, exclude_artifacts: Optional[List[str]] = []
+        project: Project, *, artifact_directory_path: str, exclude_artifacts: Optional[List[str]] = []
 ) -> Operation:
-    tamr_client = tamr_project.client
+    """Export project artifacts for project movement
+
+    Version:
+        Requires Tamr 2021.005.0 or later
+
+    Args:
+        project: a tamr project object
+        artifact_directory_path: export directory for project artifacts
+        exclude_artifacts: list of artifacts to exclude
+
+    Returns:
+        operation for project export api call
+    """
+    # check version compatibility for project movement
+    tamr_client = project.client
     utils.version.enforce_after_or_equal(
         client=tamr_client, compare_version="2021.005.0"
     )
     logging.info("Tamr version has movement compatibility.")
 
-    body = {"artifactDirectory": directory_path, "excludeArtifacts": exclude_artifacts}
-    logging.info(f"Sending query to export project, {tamr_project.name}.")
+    # make project export api request
+    body = {"artifactDirectory": artifact_directory_path, "excludeArtifacts": exclude_artifacts}
+    logging.info(f"Sending query to export project, {project.name}.")
     response = tamr_client.post(
-        f"/api/versioned/v1/projects/{tamr_project.resource_id}:export", json=body
+        f"/api/versioned/v1/projects/{project.resource_id}:export", json=body
     )
     response.successful()
+
+    # periodically check export job until completion
     job_id = response.json()["id"]
     operation = utils.operation.from_resource_id(tamr=tamr_client, job_id=job_id)
     logging.info(f"Waiting for export to be created.")
@@ -40,7 +58,8 @@ def export_project(
     return operation
 
 
-def import_new_project(
+def import_project(
+        *,
         tamr_client: Client,
         project_artifact_path: str,
         new_project_name: str = None,
@@ -49,23 +68,44 @@ def import_new_project(
         include_additive_artifacts: Optional[List[str]] = [],
         include_destructive_artifacts: Optional[List[str]] = [],
 ) -> Operation:
+    """Import project artifacts into a tamr instance
+
+    Version:
+        Requires Tamr 2021.005.0 or later
+
+    Args:
+        tamr_client: : a tamr client
+        project_artifact_path: project artifacts zip filepath
+        new_project_name: new project name
+        new_unified_dataset_name: new unified dataset name
+        exclude_artifacts: list of artifacts to exclude in import
+        include_additive_artifacts: list of artifacts to import only additively
+        include_destructive_artifacts: list of artifacts to import destructively
+
+    Returns:
+        operation for project import api call
+    """
+    # check version compatibility for project movement
     utils.version.enforce_after_or_equal(
         client=tamr_client, compare_version="2021.005.0"
     )
     logging.info("Tamr version has movement compatibility.")
 
+    # make project import api request
     body = {
-        "excludeArtifacts": exclude_artifacts,
-        "failIfNotPresent": False,
-        "includeAdditiveArtifacts": include_additive_artifacts,
-        "includeDestructiveArtifacts": include_destructive_artifacts,
+        "projectArtifact": project_artifact_path,
         "newProjectName": new_project_name,
         "newUnifiedDatasetName": new_unified_dataset_name,
-        "projectArtifact": project_artifact_path,
+        "excludeArtifacts": exclude_artifacts,
+        "includeAdditiveArtifacts": include_additive_artifacts,
+        "includeDestructiveArtifacts": include_destructive_artifacts,
+        "failIfNotPresent": False,
     }
-
+    logging.info(f"Sending query to import project, {new_project_name}.")
     response = tamr_client.post('/v1/projects:import', json=body)
     response.successful()
+
+    # periodically check import job until completion
     job_id = response.json()["id"]
     operation = utils.operation.from_resource_id(tamr=tamr_client, job_id=job_id)
     logging.info(f"Waiting for project to be imported.")
