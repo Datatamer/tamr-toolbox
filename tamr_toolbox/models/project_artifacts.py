fb6072b1bf3aebe720a940bcc506d225c67f807b
Tamiro <tamiro.villazon@tamr.com>
2021-05-14 16:26:02 -0400

    updating doc strings and .rst files for project_artifacts

--- tamr_toolbox/models/project_artifacts.py
+++ tamr_toolbox/models/project_artifacts.py
@@ -6,6 +6,11 @@ from dataclasses import dataclass
 class SchemaMappingArtifacts:
     """A dataclass representing artifact codes for Schema Mapping projects in Tamr
 
+    Args:
+        UNIFIED_ATTRIBUTES: artifact name for a schema mapping project
+        TRANSFORMATIONS: artifact name for a schema mapping project
+        SMR_MODEL: artifact name for a schema mapping project
+        RECORD_COMMENTS: artifact name for a schema mapping project
     """
 
     # Schema Mapping artifacts
@@ -19,6 +24,23 @@ class SchemaMappingArtifacts:
 class MasteringArtifacts:
     """A dataclass representing artifact codes for Mastering projects in Tamr
 
+    Args:
+        UNIFIED_ATTRIBUTES: artifact name for a Mastering project
+        TRANSFORMATIONS: artifact name for a Mastering project
+        SMR_MODEL: artifact name for a Mastering project
+        RECORD_COMMENTS: artifact name for a Mastering project
+
+        MASTERING_CONFIGURATION: artifact name for a Mastering project
+        USER_DEFINED_SIGNALS: artifact name for a Mastering project
+        MASTERING_FUNCTIONS: artifact name for a Mastering project
+        RECORD_PAIR_COMMENTS: artifact name for a Mastering project
+        RECORD_PAIR_VERIFIED_LABELS: artifact name for a Mastering project
+        RECORD_PAIR_UNVERIFIED_LABELS: artifact name for a Mastering project
+        RECORD_PAIR_ASSIGNMENTS: artifact name for a Mastering project
+        CLUSTERING_MODEL: artifact name for a Mastering project
+        PUBLISHED_CLUSTERS: artifact name for a Mastering project
+        CLUSTER_RECORD_VERIFICATIONS: artifact name for a Mastering project
+        CLUSTER_ASSIGNMENTS: artifact name for a Mastering project
     """
 
     # Schema Mapping artifacts
@@ -44,6 +66,18 @@ class MasteringArtifacts:
 class CategorizationArtifacts:
     """A dataclass representing artifact codes for Categorization projects in Tamr
 
+    Args:
+        UNIFIED_ATTRIBUTES: artifact name for a Categorization project
+        TRANSFORMATIONS: artifact name for a Categorization project
+        SMR_MODEL: artifact name for a Categorization project
+        RECORD_COMMENTS: artifact name for a Categorization project
+
+        CATEGORIZATION_CONFIGURATION: artifact name for a Categorization project
+        CATEGORIZATION_FUNCTIONS: artifact name for a Categorization project
+        CATEGORIZATION_VERIFIED_LABELS: artifact name for a Categorization project
+        CATEGORIZATION_TAXONOMIES: artifact name for a Categorization project
+        CATEGORIZATION_MODEL: artifact name for a Categorization project
+        CATEGORIZATION_FEEDBACK: artifact name for a Categorization project
     """
 
     # Schema Mapping artifacts
@@ -64,6 +98,9 @@ class CategorizationArtifacts:
 class GoldenRecordsArtifacts:
     """A dataclass representing artifact codes for Golden Records projects in Tamr
 
+    GR_CONFIGURATION: artifact name for a Golden Records project
+    GR_RULES: artifact name for a Golden Records project
+    GR_OVERRIDES: artifact name for a Golden Records project
     """
 
     # Golden Records artifacts
