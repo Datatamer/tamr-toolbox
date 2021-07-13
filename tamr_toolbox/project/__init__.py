54fd000215b928eb1910b0f2de3691a17717a091
along528 <alex.long@tamr.com>
2021-07-13 14:52:15 -0400

    Linting

--- tamr_toolbox/project/__init__.py
+++ tamr_toolbox/project/__init__.py
@@ -5,4 +5,11 @@ from . import golden_records
 from . import categorization
 from ._common import export_artifacts, import_artifacts
 
-__all__ = ["schema_mapping", "mastering", "golden_records", "categorization", "export_artifacts", "import_artifacts"]
+__all__ = [
+    "schema_mapping",
+    "mastering",
+    "golden_records",
+    "categorization",
+    "export_artifacts",
+    "import_artifacts",
+]
