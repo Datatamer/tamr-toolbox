29976f046546a421c627f6c58e2dddcfec65e604
Erin Compaan <erin.compaan@tamr.com>
2021-09-30 09:29:48 -0400

    added store_auth input to client creation fn

--- tests/utils/test_client.py
+++ tests/utils/test_client.py
@@ -25,6 +25,7 @@ def test_client_create():
     assert my_client.protocol == CONFIG["my_instance_name"]["protocol"]
     assert my_client.base_path == "/api/versioned/v1/"
     assert my_client.auth == UsernamePasswordAuth("admin", os.environ["TAMR_TOOLBOX_PASSWORD"],)
+    assert my_client.store_auth_cookie == False
 
 
 @mock_api()
@@ -37,6 +38,12 @@ def test_client_create_none_port():
 def test_invalid_credentials():
     with pytest.raises(SystemError):
         utils.client.create(**CONFIG["my_other_instance"], enforce_healthy=True)
+
+
+@mock_api()
+def test_store_auth_cookie():
+    my_client = utils.client.create(**CONFIG['my_instance_name'], store_auth_cookie=True)
+    assert my_client.store_auth_cookie == True
 
 
 @mock_api()
