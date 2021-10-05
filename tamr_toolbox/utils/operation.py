65b35736235db8cb50b29e38403ff72202bafb5a
tristan-koopman <87034802+tristan-koopman@users.noreply.github.com>
2021-10-05 11:35:45 -0400

    Update typehint in monitor

--- tamr_toolbox/utils/operation.py
+++ tamr_toolbox/utils/operation.py
@@ -131,7 +131,7 @@ def wait(
 
 
 def monitor(
-    operation: Operation, *, poll_interval_seconds: int = 1, timeout_seconds: Optional[int] = 300,
+    operation: Operation, *, poll_interval_seconds: int = 1, timeout_seconds: int = 300,
 ) -> Operation:
     """Continuously polls for this operation's server-side state and returns operation
     when there is a state change
