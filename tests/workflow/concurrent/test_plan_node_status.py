0460f1700d5139e6c46cccee052963a8a9972a5e
Clint Richardson <clint.richardson@tamr.com>
2021-10-22 16:16:42 -0400

    added test for from_op with failed op

--- tests/workflow/concurrent/test_plan_node_status.py
+++ tests/workflow/concurrent/test_plan_node_status.py
@@ -1,7 +1,7 @@
 """Tests for updating PlanNodeStatus"""
+from tamr_unify_client.operation import Operation
 
 from tamr_toolbox.workflow.concurrent import PlanNodeStatus, PlanNode
-
 from tamr_toolbox.utils.testing import mock_api
 from tamr_toolbox import utils
 from tests._common import get_toolbox_root_dir
@@ -22,6 +22,27 @@ def test_from_op():
     # now wait for it to succeed and ensure we get success back
     op = op.wait()
     assert PlanNodeStatus.from_tamr_op(op) == PlanNodeStatus.PlanNodeStatus.SUCCEEDED
+
+
+@mock_api()
+def test_from_op_failure():
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    op_json = {
+    "id": "-1",
+    "type": "NOOP",
+    "description": "test",
+    "status": {
+        "state": "FAILED",
+        "startTime": "early",
+        "endTime": "late",
+        "message": "",
+    },
+    "created": {"username": "", "time": "early", "version": "-1"},
+    "lastModified": {"username": "", "time": "late", "version": "-1"},
+    "relativeId": "operations/-1",
+    }
+    op = Operation.from_json(client, op_json)
+    assert PlanNodeStatus.from_tamr_op(op) == PlanNodeStatus.PlanNodeStatus.FAILED
 
 
 @mock_api()
