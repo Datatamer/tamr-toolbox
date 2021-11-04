aad0fdc816284fe1ffddb3f0c99303b2e09670dd
skalish <samk825@gmail.com>
2021-11-04 16:35:55 -0400

    set exact version requirement for slackclient

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ setup(
         "all": [
             "pandas>=0.21.0",
             "aiohttp==3.7.4.post0",
-            "slackclient>=2.7.2",
+            "slackclient==2.7.2",
             "responses==0.10.14",
             "google-cloud-translate==2.0.1",
         ],
         # Individual sets of dependencies
         "pandas": ["pandas>=0.21.0"],
-        "slack": ["aiohttp==3.7.4.post0", "slackclient>=2.7.2"],
+        "slack": ["aiohttp==3.7.4.post0", "slackclient==2.7.2"],
         "testing": ["responses==0.10.14"],
         "translation": ["google-cloud-translate==2.0.1"],
     },
