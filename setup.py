c97e651419606fb209d26aaf1439695be11b13d5
skalish <39866163+skalish@users.noreply.github.com>
2021-11-05 10:05:07 -0400

    TBOX-205: enforce specific version of aiohttp to avoid ci failures (#70)
    * enforce specific version of aiohttp to avoid ci failures
    
    * set exact version requirement for slackclient

--- setup.py
+++ setup.py
@@ -27,13 +27,14 @@ setup(
         # Super set including all optional dependencies
         "all": [
             "pandas>=0.21.0",
-            "slackclient>=2.7.2",
+            "aiohttp==3.7.4.post0",
+            "slackclient==2.7.2",
             "responses==0.10.14",
             "google-cloud-translate==2.0.1",
         ],
         # Individual sets of dependencies
         "pandas": ["pandas>=0.21.0"],
-        "slack": ["slackclient>=2.7.2"],
+        "slack": ["aiohttp==3.7.4.post0", "slackclient==2.7.2"],
         "testing": ["responses==0.10.14"],
         "translation": ["google-cloud-translate==2.0.1"],
     },
