7d4d518244ac05152c055eaf36cc862364301074
ErinCompaan <86741767+ErinCompaan@users.noreply.github.com>
2021-11-29 14:14:41 -0800

    TBOX-155 Rename CSV columns (#69)
    * initial column renaming functionality
    
    * adding working tests, response logs, and example
    
    * remove pdebugging print stmts
    
    * improve typehint
    
    Co-authored-by: skalish <39866163+skalish@users.noreply.github.com>
    
    * more concise dict definition
    
    Co-authored-by: skalish <39866163+skalish@users.noreply.github.com>
    
    * typo fix
    
    * expanded test and added comments
    
    Co-authored-by: skalish <39866163+skalish@users.noreply.github.com>

--- tests/data_io/test_csv.py
+++ tests/data_io/test_csv.py
@@ -1,6 +1,6 @@
 """Tests for tasks related to moving data in or out of Tamr using delimited files"""
 import pytest
-from typing import List, Optional
+from typing import List, Optional, Dict
 import io
 import os
 import tempfile
@@ -284,3 +284,75 @@ def test_bad_encoding():
     filepath = os.path.join(get_toolbox_root_dir(), "tests/data_io/temp_bad_encoding.csv")
     csv.from_dataset(dataset, filepath)
     os.remove(filepath)
+
+
+@pytest.mark.parametrize(
+    "columns, column_name_dict",
+    [
+        (
+            None,
+            {"first_name": "new_first_name", "last_name": "family_name"},
+        ),  # basic test renaming test
+        (
+            None,
+            {"not_real_col": "full_name", "last_name": "family_name"},
+        ),  # test with column name not present in the data
+        (
+            ["tamr_id", "all_names", "ssn", "last_name", "first_name"],
+            {"last_name": "family_name", "ssn": "SS", "first_name": "given_name"},
+        ),  # test with columns list -- columns order is preserved regardless of mapping dict order
+    ],
+)
+@mock_api()
+def test_renaming_csv_columns(
+    columns: List[str], column_name_dict: Dict[str, str],
+):
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    sm_dataset_id = CONFIG["datasets"]["minimal_schema_mapping_unified_dataset"]
+    dataset = client.datasets.by_resource_id(sm_dataset_id)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = Path(tempfile.gettempdir()) / f"test_export_csv_{timestamp}.csv"
+    records_written = csv.from_dataset(
+        dataset, filename, overwrite=True, columns=columns, column_name_dict=column_name_dict,
+    )
+
+    # Load raw export data and sort for comparison.
+    # Clean up default handling of multi-values where a value is empty (e.g. "|Tuck") since the
+    # toolbox does not export in this way (e.g. "Tuck")
+    compare_to_df = pd.read_csv(
+        io.StringIO(TEST_DATA), dtype="object", index_col="tamr_id"
+    ).sort_index()
+    compare_to_df["all_names"] = compare_to_df["all_names"].str.strip("|").str.split("|")
+
+    # Sort columns of test data
+    if columns is not None:
+        columns.remove("tamr_id")
+        compare_to_df = compare_to_df[columns]
+    # CSV renaming should behave the same way as pandas dataframe renaming
+    compare_to_df.rename(columns=column_name_dict, inplace=True)
+
+    test_df = pd.read_csv(
+        filename, dtype="object", delimiter=",", index_col="tamr_id", quotechar='"'
+    ).sort_index()
+    test_df["all_names"] = test_df["all_names"].str.split("|")
+
+    assert records_written == len(compare_to_df)
+    assert test_df.fillna("").isin(compare_to_df.fillna("")).all(axis=0).all()
+
+
+@mock_api()
+def test_invalid_renaming_map(
+    columns=["tamr_id", "all_names", "ssn", "last_name", "first_name"],
+    column_name_dict={"all_names": "last_name"},
+):
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    sm_dataset_id = CONFIG["datasets"]["minimal_schema_mapping_unified_dataset"]
+    dataset = client.datasets.by_resource_id(sm_dataset_id)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = Path(tempfile.gettempdir()) / f"test_export_csv_{timestamp}.csv"
+
+    # ValueError raised by renaming that would yield duplicate columns
+    with pytest.raises(ValueError):
+        csv.from_dataset(
+            dataset, filename, overwrite=True, columns=columns, column_name_dict=column_name_dict,
+        )
