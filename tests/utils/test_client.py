64792397f703347b0892d442db2f2eb8267bb974
DIvkov575 <Dmitriy@Ivkov.net>
2022-01-10 21:41:10 -0500

    added try/except for tempdir.cleanup() to fix windows fail

--- tests/utils/test_client.py
+++ tests/utils/test_client.py
@@ -59,26 +59,28 @@ def test_client_enforce_healthy():
 
 @mock_api()
 def test_get_with_connection_retry():
-    # To ensure timeout error is raised to test correct ConnectionError is caught
+
+    tempdir = tempfile.TemporaryDirectory()
 
     log_prefix = "caught_connection_error"
+    log_file_path = os.path.join(tempdir.name, f"{log_prefix}_{utils.logger._get_log_filename()}")
 
-    with tempfile.TemporaryDirectory() as tempdir:
-        log_file_path = os.path.join(tempdir, f"{log_prefix}_{utils.logger._get_log_filename()}")
 
-        if os.path.exists(log_file_path):
-            os.remove(log_file_path)
+    my_client = utils.client.create(**CONFIG["my_other_instance"])
+    utils.logger.enable_toolbox_logging(
+        log_to_terminal=False, log_directory=tempdir.name, log_prefix=log_prefix
+    )
 
-        my_client = utils.client.create(**CONFIG["my_other_instance"])
-        utils.logger.enable_toolbox_logging(
-            log_to_terminal=False, log_directory=tempdir, log_prefix=log_prefix
+    with pytest.raises(TimeoutError):
+        utils.client.get_with_connection_retry(
+            my_client, "/api/service/health", timeout_seconds=10, sleep_seconds=1
         )
 
-        with pytest.raises(TimeoutError):
-            utils.client.get_with_connection_retry(
-                my_client, "/api/service/health", timeout_seconds=10, sleep_seconds=1
-            )
+    with open(log_file_path, "r") as f:
+        # confirm that the intended warning was written to the log
+        assert "Caught exception in connect" in f.read()
 
-        with open(log_file_path, "r") as f:
-            # confirm that the intended warning was written to the log
-            assert "Caught exception in connect" in f.read()
+    try:
+        tempdir.cleanup()
+    except (PermissionError, NotADirectoryError):
+        tempdir.cleanup()
