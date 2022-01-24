f824fb501c70d24c2ba6b5695f44b889d020016a
ErinCompaan <86741767+ErinCompaan@users.noreply.github.com>
2022-01-24 17:18:49 -0500

    TBOX-202 Improve CSV test coverage (#75)
    * Rename tests for `csv.py` functions
    * Test additional parameter sets and errors raised by `csv.py`
    
    
    Co-authored-by: skalish <39866163+skalish@users.noreply.github.com>

--- tests/data_io/test_csv.py
+++ tests/data_io/test_csv.py
@@ -59,17 +59,17 @@ EMPTY_TEST_DATA = """
 "id","first_name","nickname","last_name","ssn"
 """
 
-IN_MEM_TAXONOMY_CSV = io.StringIO(
-    "Dairy\n"
-    "Dairy,Cheese\n"
-    "Dairy,Milk\n"
-    "Meat\n"
-    "Meat,Beef\n"
-    "Meat,Chicken\n"
-    "Meat,Chicken,bone-in\n"
-    "Meat,Chicken,boneless\n"
-    "Vegetables\n"
-)
+TAXONOMY_DATA = [
+    "Dairy\n",
+    "Dairy,Cheese\n",
+    "Dairy,Milk\n",
+    "Meat\n",
+    "Meat,Beef\n",
+    "Meat,Chicken\n",
+    "Meat,Chicken,bone-in\n",
+    "Meat,Chicken,boneless\n",
+    "Vegetables\n",
+]
 
 
 @pytest.mark.parametrize(
@@ -95,7 +95,7 @@ IN_MEM_TAXONOMY_CSV = io.StringIO(
     ],
 )
 @mock_api()
-def test_export_csv(
+def test_dataset_export_csv(
     buffer_size: Optional[int],
     nrows: Optional[int],
     csv_delimiter: str,
@@ -153,12 +153,45 @@ def test_export_csv(
         assert test_df.fillna("").isin(compare_to_df.fillna("")).all(axis=0).all()
 
 
+@mock_api()
+def test_dataset_export_csv_delim_error():
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    sm_dataset_id = CONFIG["datasets"]["minimal_schema_mapping_unified_dataset"]
+    dataset = client.datasets.by_resource_id(sm_dataset_id)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        filepath = Path(tempdir) / "test_taxonomy_invalid_delim.csv"
+
+        with pytest.raises(ValueError):
+            csv.from_dataset(dataset, filepath, csv_delimiter="|", flatten_delimiter="|")
+
+
+@pytest.mark.parametrize("overwrite", [False])
+@mock_api()
+def test_dataset_overwrite_file(overwrite: bool):
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    sm_dataset_id = CONFIG["datasets"]["minimal_schema_mapping_unified_dataset"]
+    dataset = client.datasets.by_resource_id(sm_dataset_id)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        filepath = Path(tempdir) / "test_dataset_overwrite_{overwrite}.csv"
+
+        f = open(filepath, "w")
+        f.write("Temporary file")
+        f.close()
+
+        with pytest.raises(FileExistsError):
+            csv.from_dataset(
+                dataset, filepath, csv_delimiter=",", flatten_delimiter="|", overwrite=overwrite
+            )
+
+
 @pytest.mark.parametrize(
-    "buffer_size, nrows, csv_delimiter, flatten_delimiter", [(None, None, ",", "|")],
+    "buffer_size, nrows, columns", [(None, None, None), (None, None, ["id", "ssn", "last_name"])],
 )
 @mock_api()
-def test_export_csv_empty_dataset(
-    buffer_size: Optional[int], nrows: Optional[int], csv_delimiter: str, flatten_delimiter: str,
+def test_dataset_export_csv_empty_dataset(
+    buffer_size: Optional[int], nrows: Optional[int], columns: Optional[List[str]],
 ):
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     empty_dataset_id = CONFIG["datasets"]["people_0_records"]
@@ -166,22 +199,18 @@ def test_export_csv_empty_dataset(
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = Path(tempfile.gettempdir()) / f"test_export_csv_{timestamp}.csv"
     records_written = csv.from_dataset(
-        dataset,
-        filename,
-        overwrite=True,
-        buffer_size=buffer_size,
-        nrows=nrows,
-        csv_delimiter=csv_delimiter,
-        flatten_delimiter=flatten_delimiter,
+        dataset, filename, overwrite=True, buffer_size=buffer_size, nrows=nrows, columns=columns
     )
+
+    header_string = EMPTY_TEST_DATA if columns is None else ",".join(f'"{col}"' for col in columns)
 
     # Load raw export data and sort for comparison.
     compare_to_df = pd.read_csv(
-        io.StringIO(EMPTY_TEST_DATA), dtype="object", index_col="id"
+        io.StringIO(header_string), dtype="object", index_col="id"
     ).sort_index()
 
     test_df = pd.read_csv(
-        filename, dtype="object", delimiter=csv_delimiter, index_col="id", quotechar='"'
+        filename, dtype="object", delimiter=",", index_col="id", quotechar='"'
     ).sort_index()
 
     assert test_df.equals(compare_to_df)
@@ -189,14 +218,14 @@ def test_export_csv_empty_dataset(
 
 
 @mock_api()
-def test_export_to_csv():
+def test_taxonomy_export_csv():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     categorization_project_id = CONFIG["projects"]["minimal_categorization"]
     project = client.projects.by_resource_id(categorization_project_id)
     filepath = os.path.join(get_toolbox_root_dir(), "tests/data_io/temp_taxonomy.csv")
     records_written = csv.from_taxonomy(project, filepath, csv_delimiter=",")
     list_written = list(io.open(filepath))
-    list_ref = list(IN_MEM_TAXONOMY_CSV)
+    list_ref = TAXONOMY_DATA
 
     assert len(list_ref) == len(list_written)
     assert len(list_ref) == records_written
@@ -207,7 +236,7 @@ def test_export_to_csv():
 
 
 @mock_api()
-def test_existing_file():
+def test_taxonomy_existing_file():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     categorization_project_id = CONFIG["projects"]["minimal_categorization"]
     project = client.projects.by_resource_id(categorization_project_id)
@@ -222,7 +251,7 @@ def test_existing_file():
 
 
 @mock_api()
-def text_overwrite_file():
+def test_taxonomy_overwrite_file():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     categorization_project_id = CONFIG["projects"]["minimal_categorization"]
     project = client.projects.by_resource_id(categorization_project_id)
@@ -233,7 +262,7 @@ def text_overwrite_file():
     f.close()
 
     records_written = csv.from_taxonomy(project, filepath, overwrite=True)
-    list_ref = list(IN_MEM_TAXONOMY_CSV)
+    list_ref = TAXONOMY_DATA
     list_written = list(io.open(filepath))
 
     assert records_written == len(list_ref)
@@ -243,7 +272,7 @@ def text_overwrite_file():
 
 
 @mock_api()
-def test_check_invalid_file_path():
+def test_taxonomy_check_invalid_file_path():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     categorization_project_id = CONFIG["projects"]["minimal_categorization"]
     project = client.projects.by_resource_id(categorization_project_id)
@@ -254,7 +283,7 @@ def test_check_invalid_file_path():
 
 
 @mock_api()
-def test_check_invalid_project_type():
+def test_taxonomy_check_invalid_project_type():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     master_project_id = CONFIG["projects"]["minimal_mastering"]
     project = client.projects.by_resource_id(master_project_id)
@@ -265,18 +294,33 @@ def test_check_invalid_project_type():
 
 
 @mock_api()
-def test_invalid_delimiters():
+def test_taxonomy_invalid_delimiters():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     categorization_project_id = CONFIG["projects"]["minimal_categorization"]
     project = client.projects.by_resource_id(categorization_project_id)
-    filepath = os.path.join(get_toolbox_root_dir(), "tests/data_io/temp_taxonomy.csv")
 
-    with pytest.raises(ValueError):
-        csv.from_taxonomy(project, filepath, csv_delimiter=",", flatten_delimiter=",")
+    with tempfile.TemporaryDirectory() as tempdir:
+        filepath = Path(tempdir) / "tests/data_io/temp_taxonomy_invalid_delim.csv"
+
+        with pytest.raises(ValueError):
+            csv.from_taxonomy(project, filepath, csv_delimiter=",", flatten_delimiter=",")
 
 
 @mock_api()
-def test_bad_encoding():
+def test_taxonomy_missing():
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    categorization_project_id = CONFIG["projects"]["categorization_no_taxonomy"]
+    project = client.projects.by_resource_id(categorization_project_id)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        filepath = Path(tempdir) / "tests/data_io/temp_taxonomy_missing.csv"
+
+        with pytest.raises(RuntimeError):
+            csv.from_taxonomy(project, filepath, csv_delimiter=",", flatten_delimiter="|")
+
+
+@mock_api()
+def test_dataset_bad_encoding():
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
     bad_encoding_dataset_id = CONFIG["datasets"]["bad_encoding"]
     dataset = client.datasets.by_resource_id(bad_encoding_dataset_id)
@@ -304,7 +348,7 @@ def test_bad_encoding():
     ],
 )
 @mock_api()
-def test_renaming_csv_columns(
+def test_dataset_renaming_csv_columns(
     columns: List[str], column_name_dict: Dict[str, str],
 ):
     client = utils.client.create(**CONFIG["toolbox_test_instance"])
@@ -341,7 +385,7 @@ def test_renaming_csv_columns(
 
 
 @mock_api()
-def test_invalid_renaming_map(
+def test_dataset_invalid_renaming_map(
     columns=["tamr_id", "all_names", "ssn", "last_name", "first_name"],
     column_name_dict={"all_names": "last_name"},
 ):
@@ -356,3 +400,18 @@ def test_invalid_renaming_map(
         csv.from_dataset(
             dataset, filename, overwrite=True, columns=columns, column_name_dict=column_name_dict,
         )
+
+
+@mock_api()
+def test_dataset_not_streamable():
+    client = utils.client.create(**CONFIG["toolbox_test_instance"])
+    sm_dataset_id = CONFIG["datasets"]["broken_schema_mapping_unified_dataset"]
+    dataset = client.datasets.by_resource_id(sm_dataset_id)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        filepath = Path(tempdir) / f"test_dataset_not_streamable_{timestamp}.csv"
+
+        # ValueError raised by renaming that would yield duplicate columns
+        with pytest.raises(RuntimeError):
+            csv.from_dataset(dataset, filepath, allow_dataset_refresh=False)
