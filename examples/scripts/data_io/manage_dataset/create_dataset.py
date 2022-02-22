01d548825afe0ac46430503b99a5589bbb280777
tristan-koopman <87034802+tristan-koopman@users.noreply.github.com>
2022-02-22 17:27:35 -0500

    Create example scripts

---
+++ examples/scripts/data_io/manage_dataset/create_dataset.py
@@ -0,0 +1,62 @@
+"""Example script for creating a dataset"""
+import argparse
+from typing import Dict, Any, List
+
+import tamr_toolbox as tbox
+from tamr_toolbox.data_io import manage_dataset
+
+
+def main(
+    *,
+    instance_connection_info: Dict[str, Any],
+    dataset_name: str,
+    attributes: List[str],
+    primary_key: str,
+    description: str,
+) -> None:
+    """Creates a dataset in Tamr
+
+    Args:
+        instance_connection_info: Information for connecting to Tamr (host, port, username etc)
+        dataset_name: name of dataset
+        attributes: list of attributes to create
+        primary_key: primary key for dataset
+        description: description of dataset
+    """
+    # Create the tamr client
+    tamr_client = tbox.utils.client.create(**instance_connection_info)
+
+    LOGGER.info(f"Creating dataset: {dataset_name}")
+
+    manage_dataset.create_dataset(
+        tamr=tamr_client,
+        dataset_name=dataset_name,
+        attributes=attributes,
+        primary_keys=primary_key,
+        description=description,
+    )
+
+
+if __name__ == "__main__":
+    # Set up command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="path to a YAML configuration file", required=False)
+    args = parser.parse_args()
+
+    # Load the configuration from the file path provided or the default file path specified
+    CONFIG = tbox.utils.config.from_yaml(
+        path_to_file=args.config,
+        default_path_to_file="examples/resources/conf/create_dataset.config.yaml",
+    )
+
+    # Use the configuration to create a global logger
+    LOGGER = tbox.utils.logger.create(__name__, log_directory=CONFIG["logging_dir"])
+
+    # Run the main function
+    main(
+        instance_connection_info=CONFIG["my_tamr_instance"],
+        dataset_name=CONFIG["datasets"]["my_source_dataset"]["name"],
+        attributes=CONFIG["datasets"]["my_source_dataset"]["attributes"],
+        primary_key=CONFIG["datasets"]["my_source_dataset"]["primary_key"],
+        description=CONFIG["datasets"]["my_source_dataset"]["description"],
+    )
---
+++ examples/snippets/data_io/manage_dataset/migrate_dataset_changes.py
@@ -0,0 +1,41 @@
+"""
+An example script to migrate changes to a dataset from one instance of Tamr to another
+"""
+import tamr_toolbox as tbox
+from tamr_toolbox.data_io import manage_dataset
+
+# load example multi config
+my_config = tbox.utils.config.from_yaml("examples/resources/conf/migrate_dataset.config.yaml")
+
+# Use the configuration to create a global logger
+LOGGER = tbox.utils.logger.create(__name__, log_directory=my_config["logging_dir"])
+
+# Create the source tamr client
+source_client = tbox.utils.client.create(**my_config["source_migration_instance"])
+
+# Create the target tamr client
+target_client = tbox.utils.client.create(**my_config["target_migration_instance"])
+
+# Update each dataset
+datasets = my_config["datasets"]
+for ds in datasets:
+    dataset_name = ds["dataset_name"]
+    primary_key = ds["primary_key"]
+
+    # Get dataset from source instance
+    source_dataset = source_client.datasets.by_name(dataset_name)
+
+    # Get updated dataset definition
+    attributes = [attr for attr in source_dataset.attributes.stream()]
+    description = source_dataset.description
+    tags = source_dataset.tags
+
+    # Migrate dataset updates from source to target instance
+    manage_dataset.modify_dataset(
+        client=target_client,
+        dataset_name=dataset_name,
+        attributes=attributes,
+        primary_keys=primary_key,
+        description=description,
+        tags=tags,
+    )
