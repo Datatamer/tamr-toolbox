e03071d4f814a2239eaca58ecce651e8baa3e10a
Amir Taghavey <a.taghavey@gmail.com>
2022-03-09 15:39:10 -0500

    implemented additional suggested edits by skalish

--- examples/snippets/utils/client/manual_auth.py
+++ examples/snippets/utils/client/manual_auth.py
@@ -1,14 +1,15 @@
 """
 Snippet for creating a tamr client object with manual passing of credentials during runtime.
+
+a. using the getpass function will conceal password phrase and requires manual human user input
+b. this approach cannot be coupled with fully automated workflows (e.g., crontab jobs)
+c. it is presumed that when setting AUTH manually the config.yaml file does not contain the
+   username and password fields
 """
+
 from tamr_toolbox import utils as tbu
 from getpass import getpass
 
-
-## a. using the getpass function will conceal password phrase and requires manual human user input
-## b. this approach cannot be coupled with fully automated workflows (e.g., crontab jobs)
-## c. it is presumed that when setting AUTH manually the config.yaml file does not contain the
-## username and password fields
 
 # load configuration variables that do not include AUTH credentials
 config_path = "."
