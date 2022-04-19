30a780bc0408431c9b8d01a76bb10a97210e522f
ataghavey <amir.taghavey@tamr.com>
2022-04-19 12:51:50 -0400

    made requested changes by skalish and ycharm to fork_project.py

--- examples/scripts/project/_common/fork_project.py
+++ examples/scripts/project/_common/fork_project.py
@@ -16,6 +16,14 @@ def export_from_tamr(client: Client, *, project_name: str, export_path: str,) ->
     This function sets path for project artifacts export from Tamr and makes the call to execute
     the export action.
     Export path defaults to "project-movement/<project_name>" if no user-defined value is passed.py
+
+    Args:
+        client: an instance of Tamr unify client object
+        project_name: name of the project to be exported from Tamr
+        export_path: export path - must be accessible to the VM hosting Tamr 
+
+    Returns:
+        operation for project export api call
     """
     project = client.projects.by_name(project_name)
     if not export_path:
@@ -25,14 +33,42 @@ def export_from_tamr(client: Client, *, project_name: str, export_path: str,) ->
     return tbox.project.export_artifacts(project=project, artifact_directory_path=export_path)
 
 
-def main(*, opts: Namespace, instance_connection_info: Dict[str, Any]):
+def main(
+    *,
+    project_name: str,
+    new_name: str,
+    postfix: str,
+    new_ud_name: str,
+    export_path: Path,
+    overwrite: bool,
+    instance_connection_info: Dict[str, Any],
+):
+    """
+    This function creates a fork copy of a Tamr project by exporting the target project 
+    and importing it back to Tamr under a new name
+
+    Example scripts for creating a copy of an existing project with the new project name:
+        - as "_copy"-postfixed name of the target project:
+            python fork_project.py --export_path /path/to/export-file-dir/ --project_name <target_project_name> --postfix _copy
+        - specified explicitly:
+            python fork_project.py --export_path /path/to/export-file-dir/ --project_name <target_project_name> --new_name <new_project_name>
+
+    Args:
+        project_name: name of the existing target project
+        new_name: name of the forked project (Optional)
+        postfix: if specified, will use and modify the target project name (Optional)
+        new_ud_name: if specified, will explicitly the name of the unified dataset of the forked project
+        export_path: export path - must be accessible to the VM hosting Tamr
+        overwrite: flag to overwrite existing project artifacts
+        instance_connection_info: Tamr instance & AUTH configs
+    """
 
     tamr_client = tbu.client.create(**instance_connection_info)
 
     # calling the action functions:
     # exporting the target project from tamr
-    LOGGER.info(f"Project {opts.name} export from Tamr initializing...")
-    op = export_from_tamr(tamr_client, project_name=opts.name, export_path=opts.exportPath)
+    LOGGER.info(f"Project {project_name} export from Tamr initializing...")
+    op = export_from_tamr(tamr_client, project_name=project_name, export_path=export_path)
     tbu.operation.enforce_success(op)
     LOGGER.info(op)
 
@@ -40,14 +76,14 @@ def main(*, opts: Namespace, instance_connection_info: Dict[str, Any]):
     # finding the path to export file
     zipfile_name = [
         f
-        for f in os.listdir(opts.exportPath)
-        if (os.path.isfile(os.path.join(opts.exportPath, f)) and f.endswith(".zip"))
+        for f in os.listdir(export_path)
+        if (os.path.isfile(os.path.join(export_path, f)) and f.endswith(".zip"))
     ][0]
-    zipfile_path = f"{opts.exportPath}/{zipfile_name}"
+    zipfile_path = f"{export_path}/{zipfile_name}"
 
     # constructing the new project name and respective unified dataset name to be imported to tamr
-    new_project_name = opts.newName if opts.newName else f"{opts.name}{opts.postfix}"
-    new_ud_name = opts.newUDName if opts.newUDName else f"{new_project_name}_unified_dataset"
+    new_project_name = new_name if new_name else f"{project_name}{postfix}"
+    new_ud_name = new_ud_name if new_ud_name else f"{new_project_name}_unified_dataset"
     # importing a copy of target project to tamr
     LOGGER.info(f"Project {new_project_name} import to Tamr initializing...")
     op = tbox.project.import_artifacts(
@@ -55,12 +91,12 @@ def main(*, opts: Namespace, instance_connection_info: Dict[str, Any]):
         project_artifact_path=zipfile_path,
         new_project_name=new_project_name,
         new_unified_dataset_name=new_ud_name,
-        overwrite_existing=opts.overwrite,
+        overwrite_existing=overwrite,
     )
     tbu.operation.enforce_success(op)
     LOGGER.info(op)
     LOGGER.info(
-        f"Project {new_project_name} was successfully forked from Tamr project {opts.name}!"
+        f"Project {new_project_name} was successfully forked from Tamr project {project_name}!"
     )
 
 
@@ -68,16 +104,16 @@ if __name__ == "__main__":
     # parse args
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--name", default=None, required=True, help="raise to specify the name of target project",
+        "--project_name", default=None, required=True, help="raise to specify the name of target project",
     )
     parser.add_argument(
-        "--exportPath",
+        "--export_path",
         default=None,
         required=True,
         help="raise to specify the path to export directory",
     )
     parser.add_argument(
-        "--newUDName",
+        "--new_ud_name",
         default=None,
         help="raise to explicitly specify the name of forked project unified dataset (optional)",
     )
@@ -89,7 +125,7 @@ if __name__ == "__main__":
     )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
-        "--newName", default=None, help="raise to explicitly specify the name of the new project",
+        "--new_name", default=None, help="raise to explicitly specify the name of the new project",
     )
     group.add_argument(
         "--postfix",
@@ -112,4 +148,12 @@ if __name__ == "__main__":
     conf_dir = "."
     config = tbu.config.from_yaml(conf_dir)
 
-    main(opts=opts, instance_connection_info=config["tamr"])
+    main(
+        project_name=opts.project_name, 
+        new_name=opts.new_name,
+        postfix=opts.postfix,
+        new_ud_name=opts.new_ud_name,
+        export_path=opts.export_path,
+        overwrite=opts.overwrite,
+        instance_connection_info=config["tamr"]
+    )
