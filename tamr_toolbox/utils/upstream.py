0c8e1da00b2bc0b95cfa1164f6700487a3d87776
Zuzana Kovacsova <94525962+Kovacsova@users.noreply.github.com>
2022-04-25 15:06:28 +0100

    TBOX-243: Return upstream datasets in utils.upstream (#98)
    * Adding return upstream dataset function + tests + docs
    
    Co-authored-by: Yasha Sharma <38109452+ycharm@users.noreply.github.com>

--- tamr_toolbox/utils/upstream.py
+++ tamr_toolbox/utils/upstream.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 import logging
 
 from tamr_unify_client.project.resource import Project
+from tamr_unify_client.dataset.resource import Dataset
 
 LOGGER = logging.getLogger(__name__)
 
@@ -94,6 +95,79 @@ def _find_projects(
         ]
 
         return upstream_projects
+
+
+def _find_datasets(dataset: Dataset) -> List[Dataset]:
+    """Returns a dataset's upstream datasets.
+
+        Args:
+            dataset: a Tamr Dataset Object
+        Returns:
+            List of upstream datasets
+
+    """
+
+    # Create empty list to populate with upstream datasets
+    data_upstream = []
+    # Main dataset
+    main_dataset = dataset
+    # Create list of datasets to go through
+    datasets_to_check = [main_dataset]
+    # Create list of checked datasets
+    datasets_checked = []
+
+    # Find all upstream datasets
+    while len(datasets_to_check) > 0:
+        # output is a list; check if anything present, add output to upstream datasets
+        # and add to list of datasets to check
+        if datasets_to_check[0].name not in datasets_checked:
+            upstream = _request_upstream_datasets(datasets_to_check[0])
+            datasets_checked.append(datasets_to_check[0].name)
+            datasets_to_check.remove(datasets_to_check[0])
+            if len(upstream) != 0:
+                data_upstream.extend(upstream)
+                datasets_to_check.extend(upstream)
+        else:
+            datasets_to_check.remove(datasets_to_check[0])
+    # Deduplicate & collect names: deduplicate based on identical names
+    dataset_names = []
+    for data in data_upstream:
+        dataset_names.append(data.name)
+    # Use dictionary to remove duplicate names
+    names_datasets_zip = dict(zip(dataset_names, data_upstream))
+    dataset_upstream = list(names_datasets_zip.values())
+    return dataset_upstream
+
+
+def _request_upstream_datasets(dataset: Dataset) -> Dataset:
+
+    """ Returns a dataset's upstream dataset
+
+        Args:
+            dataset: a Tamr Dataset Object
+        Returns:
+            The upstream datasets
+    """
+    # Find upstream datasets, output is a DatasetURI
+    upstream = dataset.upstream_datasets()
+    dataset_upstream = []
+    # Make Dataset our of DatasetURI
+    for data in upstream:
+        dataset_upstream.append(dataset.client.datasets.by_resource_id(data.resource_id))
+    return dataset_upstream
+
+
+def datasets(dataset: Dataset) -> List[Dataset]:
+    """ Check for upstream datasets associated with a specified dataset
+
+    Args:
+        dataset: the Tamr dataset for which associated upstream datasets are retrieved
+    Returns:
+            List of Tamr datasets upstream of the target dataset
+    """
+    upstream_datasets = _find_datasets(dataset)
+
+    return upstream_datasets
 
 
 def projects(project: Project) -> List[Project]:
