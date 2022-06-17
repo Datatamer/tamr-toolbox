288e42b2aefc2685340a5e3b00a328723b69ef47
Oliver Gordon <oliver.gordon@tamr.com>
2022-06-17 14:13:10 +0100

    Initial guessed attempt

--- tamr_toolbox/utils/version.py
+++ tamr_toolbox/utils/version.py
@@ -1,12 +1,15 @@
 """Tasks related to the version of Tamr instances"""
-from tamr_unify_client import Client
-from typing import Callable
-import json
 import inspect
-
+import json
 import logging
+import warnings
+from collections.abc import Callable
+
+from packaging.version import parse
+from tamr_unify_client import Client
 
 LOGGER = logging.getLogger(__name__)
+logging.captureWarnings(True)
 
 
 def current(client: Client) -> str:
@@ -51,6 +54,7 @@ def _as_float(version: str) -> float:
 
 def enforce_after_or_equal(client: Client, *, compare_version: str) -> None:
     """Raises an exception if the version of the Tamr client is before the provided compare version
+        Will be deprecated in favour of raise_warn_tamr_version()
 
     Args:
         client: Tamr client
@@ -59,13 +63,87 @@ def enforce_after_or_equal(client: Client, *, compare_version: str) -> None:
     Returns:
         None
 
+    See Also:
+        raise_warn_tamr_version
+        ensure_tamr_version
     """
+    warnings.warn("Use `raise_warn_tamr_version'",
+                  DeprecationWarning, stacklevel=2)
+
     current_version = current(client)
     if _as_float(current_version) < _as_float(compare_version):
         raise NotImplementedError(
             f"This function is not available in Tamr {current_version}. "
             f"Upgrade to Tamr {compare_version} or later to use this function."
         )
+
+
+def is_tamr_version_equal(tamr_version, exact_version):
+    return parse(exact_version) == parse(tamr_version)
+
+
+def is_tamr_version_between(tamr_version, min_version, max_version):
+    return parse(min_version) <= parse(tamr_version) <= parse(str(max_version))
+
+
+def raise_warn_tamr_version(tamr_version, min_version, max_version="9999",
+                            exact_version=False, response="error"):
+    """Check Tamr version and raise error/warn as appropriate"""
+
+    accepted_raises = ["error", "warn"]
+    if response not in accepted_raises:
+        raise ValueError(f"Response must be one of {accepted_raises}")
+
+    message = None
+    if exact_version:
+        if not is_tamr_version_equal(tamr_version, min_version):
+            message = f"Using Tamr version(s) {tamr_version}, " \
+                      f"but must be exactly {min_version}."
+
+    elif not is_tamr_version_between(tamr_version, min_version, max_version):
+        message = (f"Using Tamr version(s) {tamr_version}, "
+                   f"but must be between {min_version} <= {max_version}.")
+
+    if message and (response == "error"):
+        raise EnvironmentError(message)
+    elif message and (response == "warn"):
+        warnings.warn(message)
+
+
+def _get_tamr_versions_from_function_args(*args, **kwargs):
+    all_args = locals()
+    for arg in all_args.values():
+        if type(arg.__bases__) is Client:
+            yield current(arg.__bases__)
+
+
+def ensure_tamr_version(min_version, max_version="9999", exact_version=None):
+    """Pie decorator for Tamr version checking
+
+    Examples
+    --------
+    @ensure_tamr_version(min_version=2022.002)
+    def my_toolbox_function(tamr_dataset, *args, **kwargs)
+
+    Notes
+    -------
+    The Tamr version is only checked for arguments going into the function,
+    and not new instances of Tamr referred to within function code
+
+    See Also
+    -------
+    utils.version.is_tamr_version_equal
+    utils.version.is_tamr_version_between
+    """
+
+    def _decorator(func):
+        def _inspector(*args, **kwargs):
+            for tamr_version in _get_tamr_versions_from_function_args(*args, **kwargs):
+                raise_warn_tamr_version(tamr_version, min_version, max_version, exact_version, response="exception")
+                return func(*args, **kwargs)
+            return _inspector
+
+        return _decorator
 
 
 def _deprecated_warning(func: Callable, *, message: str) -> Callable:
