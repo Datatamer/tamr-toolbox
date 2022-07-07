9e458887da4032acad12c9d0be47ae2cd789aaf1
Dima I <79413560+DIvkov575@users.noreply.github.com>
2022-07-07 11:53:07 -0400

    Update examples/snippets/filesystem/cloud/gcs_download.py
    Co-authored-by: skalish <39866163+skalish@users.noreply.github.com>

--- examples/snippets/filesystem/cloud/gcs_download.py
+++ examples/snippets/filesystem/cloud/gcs_download.py
@@ -3,9 +3,9 @@ An example script to demonstrate the use of the gcs_download function to downloa
 """
 
 import tamr_toolbox as tbox
-from google.cloud.client import Client as GcsClient
+from google.cloud import storage
 
-gcs_client = GcsClient()
+gcs_client = storage.Client()
 
 # download file from gcs
 # download a file on GCS "gs://my-bucket/path-to-file" to "my_local_directory/my_file.txt"
