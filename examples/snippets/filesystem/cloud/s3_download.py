8ebb4f1fdf56a450453c4ef0ff0bb5b6a409cc92
Dima I <79413560+DIvkov575@users.noreply.github.com>
2022-07-07 11:55:07 -0400

    Update examples/snippets/filesystem/cloud/s3_download.py
    Co-authored-by: skalish <39866163+skalish@users.noreply.github.com>

--- examples/snippets/filesystem/cloud/s3_download.py
+++ examples/snippets/filesystem/cloud/s3_download.py
@@ -3,14 +3,14 @@ An example script to demonstrate the use of the s3_download function to download
 """
 
 import tamr_toolbox as tbox
-from boto3.session import Session
+import boto3
 
-S3Client = Session("credentials").client("s3")
+s3_client = boto3.client("s3")
 
 # download file from AWS S3
 # download a file on GCS "s3://my-bucket/path-to-file" to "my_local_directory/my_file.txt"
 tbox.filesystem.cloud.s3_download(
-    cloud_client=S3Client,
+    cloud_client=s3_client,
     source_filepath="path-to-file",
     destination_filepath="my_local_directory/my_file.txt",
     bucket_name="my-bucket",
