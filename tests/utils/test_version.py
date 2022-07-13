c5a32b26d45d63e783c0a5ad9229cf7a99a3a6b8
Oliver Gordon <35759308+OGordon100@users.noreply.github.com>
2022-07-13 21:26:44 +0100

    Tbox 256 create a decorator for applying utils version enforce after or equal (#105)

--- tests/utils/test_version.py
+++ tests/utils/test_version.py
@@ -1,6 +1,7 @@
 """Tests for tasks related to the version of Tamr instances"""
-import pytest
 from typing import Union, Type
+
+import pytest
 
 from tamr_toolbox.utils import version, config, client
 from tamr_toolbox.utils.testing import mock_api
@@ -55,3 +56,96 @@ def test_enforce_after_or_equal(required_version: str, expected_pass_enforcement
         assert expected_pass_enforcement is True
     except NotImplementedError:
         assert expected_pass_enforcement is False
+
+
+def test_is_version_condition_met():
+    assert version.is_version_condition_met(
+        tamr_version="2022.002.0", min_version="2022.002.0", exact_version=True
+    )
+    assert not version.is_version_condition_met(
+        tamr_version="2022.002.0", min_version="2023.002.0", exact_version=True
+    )
+    assert not version.is_version_condition_met(
+        tamr_version="2022.002.0", min_version="2021.002.0", exact_version=True
+    )
+
+    assert version.is_version_condition_met(tamr_version="2022.002.0", min_version="2021.002.0")
+    assert not version.is_version_condition_met(
+        tamr_version="2022.002.0", min_version="2023.002.0"
+    )
+
+    assert version.is_version_condition_met(
+        tamr_version="2022.002.0", min_version="2021.002.0", max_version="2023.002.0"
+    )
+    assert not version.is_version_condition_met(
+        tamr_version="2020.002.0", min_version="2021.002.0", max_version="2023.002.0"
+    )
+    assert not version.is_version_condition_met(
+        tamr_version="2024.002.0", min_version="2021.002.0", max_version="2023.002.0"
+    )
+
+    with pytest.raises(ValueError):
+        version.is_version_condition_met(
+            tamr_version="2019.003.0", min_version="2021.003.0", max_version="2019.003.0"
+        )
+    with pytest.raises(EnvironmentError):
+        version.is_version_condition_met(
+            tamr_version="2022.002.0",
+            min_version="2021.002.0",
+            exact_version=True,
+            raise_error=True,
+        )
+
+
+@mock_api()
+def test_get_tamr_versions_from_function_args():
+    tamr_client = client.create(**CONFIG["toolbox_test_instance"])
+    tamr_project = tamr_client.projects.by_resource_id(CONFIG["projects"]["minimal_mastering"])
+    tamr_dataset = tamr_client.datasets.by_resource_id(CONFIG["datasets"]["groceries_tiny"])
+
+    assert version._get_tamr_versions_from_function_args(5, "words", a=["other_types"]) == []
+
+    assert version._get_tamr_versions_from_function_args(tamr_client) == [
+        version.current(tamr_client)
+    ]
+    assert version._get_tamr_versions_from_function_args(tamr_project) == [
+        version.current(tamr_client)
+    ]
+    assert version._get_tamr_versions_from_function_args(tamr_dataset) == [
+        version.current(tamr_client)
+    ]
+
+    assert version._get_tamr_versions_from_function_args(
+        tamr_client, tamr_project, tamr_dataset, "a"
+    ) == [version.current(tamr_client), version.current(tamr_client), version.current(tamr_client)]
+
+
+@mock_api()
+def test_ensure_tamr_version_decorator():
+    tamr_client = client.create(**CONFIG["toolbox_test_instance"])
+
+    try:
+
+        @version.requires_tamr_version(min_version="2019.001")
+        def test_function_irrelevant_inputs(*args, **kwargs):
+            pass
+
+        test_function_irrelevant_inputs(1, "2", three="stuff")
+    except Exception as exc:
+        assert False, f"Raised an exception {exc}"
+    try:
+
+        @version.requires_tamr_version(min_version="2019.001")
+        def test_function_args_passing(tamr_client, *args, **kwargs):
+            pass
+
+        test_function_args_passing(tamr_client)
+    except Exception as exc:
+        assert False, f"Raised an exception {exc}"
+    with pytest.raises(EnvironmentError):
+
+        @version.requires_tamr_version(min_version="2099.001")
+        def test_function_args_failing(tamr_client, *args, **kwargs):
+            pass
+
+        test_function_args_failing(tamr_client)
