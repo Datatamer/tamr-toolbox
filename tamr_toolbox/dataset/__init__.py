b6267608d8dc0f9bd1b792894b48b6142e0c0c7f
zbpvarun-tamr <varun.saravanan@tamr.com>
2022-07-15 10:47:59 -0400

    TBOX-144: Fixed formatting

--- tamr_toolbox/dataset/__init__.py
+++ tamr_toolbox/dataset/__init__.py
@@ -1,7 +1,5 @@
 """Tests for creating or modifying datasets in Tamr"""
 from . import manage
-from tamr_toolbox.dataset._profile import (
-    get_profile
-)
+from tamr_toolbox.dataset._profile import get_profile
 
 __all__ = ["manage"]
