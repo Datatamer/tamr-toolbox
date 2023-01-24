82a8e340b7689bb23dd2b16424a20f25e8ed2be7
Shalini Priya <97606985+shalinipriya0965@users.noreply.github.com>
2023-01-24 10:02:56 +0000

    remove trailing white spaces

--- tamr_toolbox/data_io/df_connect/client.py
+++ tamr_toolbox/data_io/df_connect/client.py
@@ -161,9 +161,9 @@ def _get_query_config(jdbc_info: jdbc_info.JdbcInfo) -> Dict:
 
 
 def _get_export_config(
-    multi_value_delimiter: str = "|", 
-    limit_records: int = 0, 
-    columns_exclude_regex: str = "", 
+    multi_value_delimiter: str = "|",
+    limit_records: int = 0,
+    columns_exclude_regex: str = "",
     renameFields: Dict[str, str] = None
 ) -> Dict:
     """Packages relevant pieces of JdbcExportInfo object into an exportDataConfig for jdbc export
