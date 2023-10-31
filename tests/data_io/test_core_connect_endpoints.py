28b38c66667474aa105e946f1d62595e701e70ed
Melissa P <melissapanggwu@gmail.com>
2023-10-31 14:56:25 -0400

    modify test

--- tests/data_io/test_core_connect_endpoints.py
+++ tests/data_io/test_core_connect_endpoints.py
@@ -7,17 +7,17 @@ from tamr_toolbox.utils.testing import mock_api
 from tests._common import get_toolbox_root_dir
 import os
 
-os.environ.setdefault("CONNECT_TOOLBOX_POSTGRES_JDBC", "jdbc:postgresql://localhost:5432/doit")
-os.environ.setdefault("CONNECT_TOOLBOX_DB_PASSWORD", "12345")
-os.environ.setdefault("TAMR_TOOLBOX_HOST", "10.20.0.250")
-os.environ.setdefault("TAMR_TOOLBOX_PASSWORD", "8&j0DhqrLrOF")
+os.environ.setdefault("CONNECT_TOOLBOX_POSTGRES_JDBC", "not/real/url")
+os.environ.setdefault("CONNECT_TOOLBOX_DB_PASSWORD", "not a real password")
+os.environ.setdefault("TAMR_TOOLBOX_HOST", "1.2.3.4")
+os.environ.setdefault("TAMR_TOOLBOX_PASSWORD", "not a real password")
 
 CONFIG = utils.config.from_yaml(
     get_toolbox_root_dir() / "tests/mocking/resources/toolbox_test.yaml"
 )
 
 
-@mock_api(enforce_online_test=True)
+@mock_api()
 def test_ingest():
     my_ingest_connect = client.from_config(CONFIG)
     print(my_ingest_connect)
@@ -28,7 +28,7 @@ def test_ingest():
     )
 
 
-@mock_api(enforce_online_test=True)
+@mock_api()
 def test_ingest_set_pk():
     my_ingest_connect = client.from_config(CONFIG)
     assert client.ingest_dataset(
@@ -39,24 +39,7 @@ def test_ingest_set_pk():
     )
 
 
-# @mock_api(enforce_online_test=True)
-# def test_export():
-#     my_export_connect = client.from_config(CONFIG)
-#     assert client.export_dataset(
-#         my_export_connect,
-#         dataset_name="test_df_connect",
-#         target_table_name="test",
-#         truncate_before_load=True,
-#     )
-
-
-# @mock_api()
-# def test_execute():
-#     my_export_connect = client.from_config(CONFIG)
-#     assert client.execute_statement(my_export_connect, "select * from test")
-#
-#
-@mock_api(enforce_online_test=True)
+@mock_api()
 def test_profile():
     my_profile_connect = client.from_config(CONFIG)
     assert client.profile_query_results(
@@ -67,25 +50,4 @@ def test_profile():
             "select * from dataset.attribute_ns_current limit 100",
         ],
     )
-#
-#
-# @mock_api()
-# def test_local_fs_avro_export():
-#     my_export_connect = client.from_config(CONFIG)
-#     assert client.export_dataset_as_avro(
-#         my_export_connect,
-#         url="/home/ubuntu/connect_export/data/people_tiny.avro",
-#         dataset_name="people_tiny.csv",
-#         fs_type=FileSystemType.LOCAL,
-#     )
-#
-#
-# @mock_api()
-# def test_local_fs_avro_schema_export():
-#     my_export_connect = client.from_config(CONFIG)
-#     assert client.export_dataset_avro_schema(
-#         my_export_connect,
-#         url="/home/ubuntu/connect_export/schema/people_tiny.avsc",
-#         dataset_name="people_tiny.csv",
-#         fs_type=FileSystemType.LOCAL,
-#     )
+
