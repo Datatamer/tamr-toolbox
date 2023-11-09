3e4f42eaefbdd355a9bd60232a1c63bfd282e6cf
Melissa P <melissapanggwu@gmail.com>
2023-11-09 10:28:52 -0500

    fix lint errors

--- tamr_toolbox/data_io/core_connect.py
+++ tamr_toolbox/data_io/core_connect.py
@@ -6,24 +6,21 @@ from tamr_unify_client import Client
 from tamr_toolbox.utils import version
 import os
 
-os.environ.setdefault(
-    "TAMR_MIN_VERSION_W_CORE_CONNECT",
-    "2022.005.0",
-)
+os.environ.setdefault("TAMR_MIN_VERSION_W_CORE_CONNECT", "2022.005.0")
 
 LOGGER = logging.getLogger(__name__)
 
 
 def ingest_dataset(
-        *,
-        client: Client,
-        jdbc_connect: JsonDict,
-        dataset_name: str,
-        query: str,
-        primary_key=None,
-        truncate_tamr_dataset=None,
-        retrieve_connect_metadata=None,
-        retrieve_source_metadata=None,
+    *,
+    client: Client,
+    jdbc_connect: JsonDict,
+    dataset_name: str,
+    query: str,
+    primary_key=None,
+    truncate_tamr_dataset=None,
+    retrieve_connect_metadata=None,
+    retrieve_source_metadata=None,
 ) -> JsonDict:
     """
     Ingest a dataset into Tamr via core_connect given query config, dataset name, query string,
@@ -44,8 +41,8 @@ def ingest_dataset(
     """
     # Check Tamr version
     if version.is_version_condition_met(
-            tamr_version=version.current(client),
-            min_version=os.environ['TAMR_MIN_VERSION_W_CORE_CONNECT'],
+        tamr_version=version.current(client),
+        min_version=os.environ["TAMR_MIN_VERSION_W_CORE_CONNECT"],
     ):
         LOGGER.info(
             f"Tamr version is equal to or after {os.environ['TAMR_MIN_VERSION_W_CORE_CONNECT']}",
@@ -76,9 +73,7 @@ def ingest_dataset(
         },
     }
 
-    LOGGER.info(
-        f"Streaming data from {jdbc_connect['jdbcUrl']} to {dataset_name}.",
-    )
+    LOGGER.info(f"Streaming data from {jdbc_connect['jdbcUrl']} to {dataset_name}.",)
     # Initiate ingestion
     response = client.post(api_path, json=ingest_body)
     response_dict = json.loads(response.content)
@@ -88,8 +83,6 @@ def ingest_dataset(
         LOGGER.error(error_message)
         raise Exception(error_message)
     else:
-        LOGGER.info(
-            f"Dataset {dataset_name} is ingested successfully.",
-        )
+        LOGGER.info(f"Dataset {dataset_name} is ingested successfully.",)
 
     return
