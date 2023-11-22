e0e2c30e897c7709ba0d91345a52695b5159e642
Erin Compaan <erin.compaan@tamr.com>
2023-11-22 12:03:13 -0500

    get tests working

--- tests/notifications/test_emails.py
+++ tests/notifications/test_emails.py
@@ -56,172 +56,167 @@ def test_build_message_with_cc_bcc():
         bcc=CONFIG["my_email_notification"]["bcc_addresses"],
     )
 
-    print(msg.as_string())
-
     assert msg.as_string() == test_response
 
 
-# def test_send_email_succeed():
-#     with patch("smtplib.SMTP_SSL", autospec=True) as mock_smtp:
-#         test_message = "This is a test email."
-#         subject_line = "Test"
+def test_send_email_succeed():
+    with patch("smtplib.SMTP_SSL", autospec=True) as mock_smtp:
+        test_message = "This is a test email."
+        subject_line = "Test"
 
-#         response = tbox.notifications.emails.send_email(
-#             message=test_message,
-#             subject_line=subject_line,
-#             sender_address=CONFIG["my_email_notification"]["sender_address"],
-#             sender_password=CONFIG["my_email_notification"]["sender_password"],
-#             recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
-#             smtp_server=CONFIG["my_email_notification"]["smtp_server"],
-#             smtp_port=CONFIG["my_email_notification"]["smtp_port"],
-#             use_tls=False,
-#         )
+        response = tbox.notifications.emails.send_email(
+            message=test_message,
+            subject_line=subject_line,
+            sender_address=CONFIG["my_email_notification"]["sender_address"],
+            sender_password=CONFIG["my_email_notification"]["sender_password"],
+            recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
+            smtp_server=CONFIG["my_email_notification"]["smtp_server"],
+            smtp_port=CONFIG["my_email_notification"]["smtp_port"],
+            use_tls=False,
+        )
 
-#         # test smptp server was created
-#         mock_smtp.assert_called()
+        # test smptp server was created
+        mock_smtp.assert_called()
 
-#         # test login function was called
-#         context = mock_smtp.return_value.__enter__.return_value
-#         context.login.assert_called()
+        # test login function was called
+        context = mock_smtp.return_value.__enter__.return_value
+        context.login.assert_called()
 
-#         # test smtplib sendmail function was called with correct parameters
-#         msg = tbox.notifications.emails._build_message(
-#             message=test_message,
-#             subject_line=subject_line,
-#             sender=CONFIG["my_email_notification"]["sender_address"],
-#             to=CONFIG["my_email_notification"]["recipient_addresses"],
-#         )
-#         context.sendmail.assert_called_with(
-#             CONFIG["my_email_notification"]["sender_address"],
-#             CONFIG["my_email_notification"]["recipient_addresses"],
-#             msg,
-#         )
-
-#         assert response["message"] == test_message
+        # test smtplib sendmail function was called with correct parameters
+        msg = tbox.notifications.emails._build_message(
+            message=test_message,
+            subject_line=subject_line,
+            sender=CONFIG["my_email_notification"]["sender_address"],
+            to=CONFIG["my_email_notification"]["recipient_addresses"],
+        )
+        context.send_message.assert_called_once()
+        args, _ = context.send_message.call_args
+        assert args[0].as_string() == msg.as_string()
+        assert response["message"] == test_message
 
 
-# def test_send_email_tls():
-#     with patch("smtplib.SMTP", autospec=True) as mock_smtp:
-#         test_message = "This is a test email."
-#         subject_line = "Test"
+def test_send_email_tls():
+    with patch("smtplib.SMTP", autospec=True) as mock_smtp:
+        test_message = "This is a test email."
+        subject_line = "Test"
 
-#         tbox.notifications.emails.send_email(
-#             message=test_message,
-#             subject_line=subject_line,
-#             sender_address=CONFIG["my_email_notification"]["sender_address"],
-#             sender_password=CONFIG["my_email_notification"]["sender_password"],
-#             recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
-#             smtp_server=CONFIG["my_email_notification"]["smtp_server"],
-#             smtp_port=CONFIG["my_email_notification"]["tls_port"],
-#             use_tls=True,
-#         )
+        tbox.notifications.emails.send_email(
+            message=test_message,
+            subject_line=subject_line,
+            sender_address=CONFIG["my_email_notification"]["sender_address"],
+            sender_password=CONFIG["my_email_notification"]["sender_password"],
+            recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
+            smtp_server=CONFIG["my_email_notification"]["smtp_server"],
+            smtp_port=CONFIG["my_email_notification"]["tls_port"],
+            use_tls=True,
+        )
 
-#         # test smptp server was created
-#         mock_smtp.assert_called()
+        # test smptp server was created
+        mock_smtp.assert_called()
 
-#         # test login function was called
-#         context = mock_smtp.return_value.__enter__.return_value
-#         context.starttls.assert_called()
-#         context.login.assert_called()
+        # test login function was called
+        context = mock_smtp.return_value.__enter__.return_value
+        context.starttls.assert_called()
+        context.login.assert_called()
 
-#         # test smtplib sendmail function was called with correct parameters
-#         msg = tbox.notifications.emails._build_message(
-#             message=test_message,
-#             subject_line=subject_line,
-#             sender=CONFIG["my_email_notification"]["sender_address"],
-#             to=CONFIG["my_email_notification"]["recipient_addresses"],
-#         )
-#         context.sendmail.assert_called_with(
-#             CONFIG["my_email_notification"]["sender_address"],
-#             CONFIG["my_email_notification"]["recipient_addresses"],
-#             msg,
-#         )
+        # test smtplib sendmail function was called with correct parameters
+        msg = tbox.notifications.emails._build_message(
+            message=test_message,
+            subject_line=subject_line,
+            sender=CONFIG["my_email_notification"]["sender_address"],
+            to=CONFIG["my_email_notification"]["recipient_addresses"],
+        )
+        context.send_message.assert_called_once()
+        args, _ = context.send_message.call_args
+        assert args[0].as_string() == msg.as_string()
 
 
-# @mock_api()
-# def test_monitor_job_succeed():
-#     with patch("smtplib.SMTP_SSL", autospec=True) as mock_smtp:
-#         client = utils.client.create(**CONFIG["my_instance_name"])
-#         project = client.projects.by_resource_id(
-#             CONFIG["projects"]["minimal_schema_mapping"]
-#         )
-#         op = project.unified_dataset().refresh(asynchronous=True)
+@mock_api()
+def test_monitor_job_succeed():
+    with patch("smtplib.SMTP_SSL", autospec=True) as mock_smtp:
+        client = utils.client.create(**CONFIG["my_instance_name"])
+        project = client.projects.by_resource_id(
+            CONFIG["projects"]["minimal_schema_mapping"]
+        )
+        op = project.unified_dataset().refresh(asynchronous=True)
 
-#         list_responses = tbox.notifications.emails.monitor_job(
-#             tamr=client,
-#             sender_address=CONFIG["my_email_notification"]["sender_address"],
-#             sender_password=CONFIG["my_email_notification"]["sender_password"],
-#             recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
-#             smtp_server=CONFIG["my_email_notification"]["smtp_server"],
-#             smtp_port=CONFIG["my_email_notification"]["smtp_port"],
-#             operation=op,
-#             poll_interval_seconds=0.01,
-#         )
+        list_responses = tbox.notifications.emails.monitor_job(
+            tamr=client,
+            sender_address=CONFIG["my_email_notification"]["sender_address"],
+            sender_password=CONFIG["my_email_notification"]["sender_password"],
+            recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
+            smtp_server=CONFIG["my_email_notification"]["smtp_server"],
+            smtp_port=CONFIG["my_email_notification"]["smtp_port"],
+            bcc_addresses=CONFIG["my_email_notification"]["bcc_addresses"],
+            operation=op,
+            poll_interval_seconds=0.01,
+        )
 
-#         # test smptp server was created
-#         mock_smtp.assert_called()
+        # test smptp server was created
+        mock_smtp.assert_called()
 
-#         expected_messages = [
-#             (
-#                 f"Host: {client.host} \n Job: {op.resource_id} \n "
-#                 f"Description: Materialize views "
-#                 f"[minimal_schema_mapping_unified_dataset] to Elastic \n Status: PENDING "
-#             ),
-#             (
-#                 f"Host: {client.host} \n Job: {op.resource_id} \n Description: Materialize "
-#                 f"views [minimal_schema_mapping_unified_dataset] to Elastic \n "
-#                 f"Status: RUNNING "
-#             ),
-#             (
-#                 f"Host: {client.host} \n Job: {op.resource_id} \n Description: Materialize "
-#                 f"views [minimal_schema_mapping_unified_dataset] to Elastic \n "
-#                 f"Status: SUCCEEDED "
-#             ),
-#         ]
+        expected_messages = [
+            (
+                f"Host: {client.host} \n Job: {op.resource_id} \n "
+                f"Description: Materialize views "
+                f"[minimal_schema_mapping_unified_dataset] to Elastic \n Status: PENDING "
+            ),
+            (
+                f"Host: {client.host} \n Job: {op.resource_id} \n Description: Materialize "
+                f"views [minimal_schema_mapping_unified_dataset] to Elastic \n "
+                f"Status: RUNNING "
+            ),
+            (
+                f"Host: {client.host} \n Job: {op.resource_id} \n Description: Materialize "
+                f"views [minimal_schema_mapping_unified_dataset] to Elastic \n "
+                f"Status: SUCCEEDED "
+            ),
+        ]
 
-#         # test that monitor job messages are expected
-#         messages = [resp["message"] for resp in list_responses]
-#         assert messages == expected_messages
+        # test that monitor job messages are expected
+        messages = [resp["message"] for resp in list_responses]
+        assert messages == expected_messages
 
 
-# @mock_api()
-# def test_monitor_job_timeout():
-#     with patch("smtplib.SMTP_SSL", autospec=True) as mock_smtp:
-#         client = utils.client.create(**CONFIG["my_instance_name"])
-#         project = client.projects.by_resource_id(
-#             CONFIG["projects"]["minimal_schema_mapping"]
-#         )
-#         op = project.unified_dataset().refresh(asynchronous=True)
+@mock_api()
+def test_monitor_job_timeout():
+    with patch("smtplib.SMTP_SSL", autospec=True) as mock_smtp:
+        client = utils.client.create(**CONFIG["my_instance_name"])
+        project = client.projects.by_resource_id(
+            CONFIG["projects"]["minimal_schema_mapping"]
+        )
+        op = project.unified_dataset().refresh(asynchronous=True)
 
-#         timeout_seconds = 0.02
-#         list_responses = tbox.notifications.emails.monitor_job(
-#             tamr=client,
-#             sender_address=CONFIG["my_email_notification"]["sender_address"],
-#             sender_password=CONFIG["my_email_notification"]["sender_password"],
-#             recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
-#             smtp_server=CONFIG["my_email_notification"]["smtp_server"],
-#             smtp_port=CONFIG["my_email_notification"]["smtp_port"],
-#             operation=op,
-#             poll_interval_seconds=0.01,
-#             timeout_seconds=timeout_seconds,
-#         )
+        timeout_seconds = 0.02
+        list_responses = tbox.notifications.emails.monitor_job(
+            tamr=client,
+            sender_address=CONFIG["my_email_notification"]["sender_address"],
+            sender_password=CONFIG["my_email_notification"]["sender_password"],
+            recipient_addresses=CONFIG["my_email_notification"]["recipient_addresses"],
+            smtp_server=CONFIG["my_email_notification"]["smtp_server"],
+            smtp_port=CONFIG["my_email_notification"]["smtp_port"],
+            cc_addresses=CONFIG["my_email_notification"]["cc_addresses"],
+            operation=op,
+            poll_interval_seconds=0.01,
+            timeout_seconds=timeout_seconds,
+        )
 
-#         # test smptp server was created
-#         mock_smtp.assert_called()
+        # test smptp server was created
+        mock_smtp.assert_called()
 
-#         expected_messages = [
-#             (
-#                 f"Host: {client.host} \n Job: {op.resource_id} \n "
-#                 f"Description: Materialize views "
-#                 f"[minimal_schema_mapping_unified_dataset] to Elastic \n Status: PENDING "
-#             ),
-#             (
-#                 f"The job {op.resource_id}: Materialize views "
-#                 f"[minimal_schema_mapping_unified_dataset]"
-#                 f" to Elastic took longer than {timeout_seconds} seconds to resolve."
-#             ),
-#         ]
+        expected_messages = [
+            (
+                f"Host: {client.host} \n Job: {op.resource_id} \n "
+                f"Description: Materialize views "
+                f"[minimal_schema_mapping_unified_dataset] to Elastic \n Status: PENDING "
+            ),
+            (
+                f"The job {op.resource_id}: Materialize views "
+                f"[minimal_schema_mapping_unified_dataset]"
+                f" to Elastic took longer than {timeout_seconds} seconds to resolve."
+            ),
+        ]
 
-#         # test that monitor job messages are expected
-#         messages = [resp["message"] for resp in list_responses]
-#         assert messages == expected_messages
+        # test that monitor job messages are expected
+        messages = [resp["message"] for resp in list_responses]
+        assert messages == expected_messages
