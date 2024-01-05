f759279f57b83f14e834cc554256516d162a5391
ErinCompaan <86741767+ErinCompaan@users.noreply.github.com>
2024-01-05 09:31:08 -0500

    TBOX-326: Update concurrent workflow graph to include independent projects (#172)
    * Update project dependency graph logic
    
    * Update/extend tests
    
    * fix typo in docstring
    
    * Revert testing comments
    
    * Add response logs
    
    * fix edges propagation through _build_edges
    
    * linting fixes

--- tamr_toolbox/workflow/concurrent/Graph.py
+++ tamr_toolbox/workflow/concurrent/Graph.py
@@ -1,31 +1,21 @@
 import logging
-from typing import List, Set, Tuple
+from collections import defaultdict
+from typing import Dict, List, Set, Tuple
 
-from dataclasses import dataclass
 import networkx as nx
-
 from tamr_unify_client import Client
 from tamr_unify_client.project.resource import Project
+from typing_extensions import DefaultDict
+
 from tamr_toolbox.models.project_type import ProjectType
-from typing import Dict, Any
 
 LOGGER = logging.getLogger(__name__)
-
-
-@dataclass()
-class Graph:
-    """
-    A dataclass for holding a set of Tamr project dependencies (edges),
-    and the generated graph from them.
-    """
-
-    edges: set
-    directed_graph: nx.DiGraph
 
 
 def _get_upstream_projects(project: Project, *, all_projects: List[Project]) -> List[Project]:
     """
     get projects immediately upstream of a given project
+
     Args:
         project: the project to check
         all_projects: a list of all Projects on the Tamr Core instance
@@ -60,7 +50,7 @@ def _get_upstream_projects(project: Project, *, all_projects: List[Project]) -> 
 
 
 def _build_edges(
-    project: Project, client: Client, *, edges: set = None, all_projects: List[Project]
+    project: Project, client: Client, *, edges: Set[Tuple[str, str]], all_projects: List[Project]
 ) -> Set[Tuple[str, str]]:
     """
     builds a set of tuples of all edges of format (source, target)
@@ -71,12 +61,10 @@ def _build_edges(
         all_projects: a list of all Projects on the Tamr Core instance
 
     Returns:
+        set of tuples describing dependencies in the form (upstream_project_name, project_name)
 
     """
     upstream_projects = _get_upstream_projects(project, all_projects=all_projects)
-
-    if edges is None:
-        edges = set()
 
     for upstream_project in upstream_projects:
         # add the edge for this upstream dataset
@@ -89,138 +77,115 @@ def _build_edges(
                 f" in edges as target: {[ x for x in edges if x[1] ==upstream_project.name]}"
             )
             continue
-        else:
-            # and then go to it and get it's upstream datasets
-            further_upstream_edges = _build_edges(
-                upstream_project, client, edges=set(edges), all_projects=all_projects
-            )
-            # print(f"adding further upstream edges {further_upstream_edges}")
-            edges = edges.union(further_upstream_edges)
+
+        # and then go to it and get its upstream datasets
+        further_upstream_edges = _build_edges(
+            upstream_project, client, edges=edges, all_projects=all_projects
+        )
+        edges = edges.union(further_upstream_edges)
 
     return edges
 
 
-def from_edges(edges: Set[tuple]) -> Graph:
+def from_project_list(projects: List[Project], client: Client) -> nx.DiGraph:
     """
-    Directly build a graph from a list of edges - tuples of format (source, target) dependencies
-
+    Creates a graph from a list of projects
     Args:
-        edges: List of edges in tuple format
-
-    Returns:
-        Graph object
-    """
-    graph = nx.DiGraph()
-    graph.add_edges_from(edges)
-    return Graph(edges=edges, directed_graph=graph)
-
-
-def from_project_list(projects: List[Project], client: Client) -> Graph:
-    """
-    Creates a graph from a list of datasets
-    Args:
-        projects: list of Tamr dataset objects
+        projects: list of Tamr project objects
         client: tamr client
 
     Returns:
-        A Graph object built from the dependencies of the datasets passed
+        A nx.DiGraph object built from the dependencies of the projects passed
     """
     # start with empty set
-    graph_edges = set()
+    graph_edges: Set[Tuple[str, str]] = set()
     # save all projects to list so we don't have to hit the API every time
     all_projects = [x for x in client.projects.stream()]
-    # for each dataset get the edges and take union
+    # for each project get the edges and take union
     for project in projects:
-        graph_edges = set(
-            graph_edges.union(_build_edges(project, client, all_projects=all_projects))
-        )
+        graph_edges = _build_edges(project, client, edges=graph_edges, all_projects=all_projects)
 
-    graph = nx.DiGraph()
-    graph.add_edges_from(graph_edges)
-    return Graph(edges=graph_edges, directed_graph=graph)
+    graph = nx.DiGraph(graph_edges)
+    graph.add_nodes_from([p.name for p in projects])  # add nodes to ensure singletons are retained
+    return graph
 
 
-def get_source_nodes(graph: Graph) -> List[str]:
+def get_source_nodes(graph: nx.DiGraph) -> List[str]:
     """
     Gives all source nodes in a graph
 
     Args:
-        graph: Graph for which to find source nodes
+        graph: nx.DiGraph for which to find source nodes
 
     Returns:
         List of node names
     """
-    di_graph = graph.directed_graph
     source_nodes = []
-    for n in di_graph.nodes():
-        if not [x for x in di_graph.predecessors(n)]:
+    for n in graph.nodes():
+        if not [x for x in graph.predecessors(n)]:
             source_nodes.append(n)
     return source_nodes
 
 
-def get_end_nodes(graph: Graph):
+def get_end_nodes(graph: nx.DiGraph) -> Set[str]:
     """
     Returns all end nodes in a directed graph
 
     Args:
-        graph: Graph for which to find end nodes
+        graph: the graph for which to find end nodes
 
     Returns:
-        List of names of all end nodes
+        Set of names of all end nodes
     """
-    di_graph = graph.directed_graph
-    end_nodes = []
-    for n in di_graph.nodes():
-        if not [x for x in di_graph.successors(n)]:
-            end_nodes.append(n)
+    end_nodes: Set[str] = set()
+    for n in graph.nodes():
+        if not [x for x in graph.successors(n)]:
+            end_nodes.add(n)
     return end_nodes
 
 
-def get_projects_by_tier(graph: Graph) -> Dict[int, Any]:
+def get_projects_by_tier(graph: nx.DiGraph) -> Dict[int, Set[str]]:
     """
     Find the different projects at each tier
 
     Args:
-        graph: the Graph for which to generate the tiers
+        graph: the nx.DiGraph for which to generate the tiers
 
     Returns:
-        A json dict who's structure is {'tier': [paths_at_that_tier], ...}
-        e.g. {1: ['SM_project_1', 'Classification_project_1'], 2: ['Mastering_project'],
-        3: ['Golden_records_project']}
+        A json dict who's structure is {'tier': {projects_at_that_tier}, ...}
+        e.g. {1: {'SM_project_1', 'Classification_project_1'}, 2: {Mastering_project'},
+        3: {'Golden_records_project'}}
     """
     source_nodes = get_source_nodes(graph)
 
-    path_tier_dict = {0: []}
-    for n in graph.directed_graph.nodes:
+    path_tier_dict: DefaultDict[int, Set[str]] = defaultdict(set)
+
+    for n in graph.nodes():
         # just add source nodes to tier 0
         if n in source_nodes:
-            path_tier_dict[0].append(n)
+            path_tier_dict[0].add(n)
             continue
         # since all dependent projects must be run first
         # the tier is the maximum length of all simple paths to this node
         # so get the max length from all source nodes
         tier = 0
         for s in source_nodes:
-            current_paths = [x for x in nx.all_simple_paths(graph.directed_graph, s, n)]
-            # skip source nodes that don't link to this project
+            current_paths = [x for x in nx.all_simple_paths(graph, s, n)]
+            # skip source nodes that don't link to this project n
             if not current_paths:
                 continue
-            max_path_length = len(max(current_paths, key=lambda x: len(x)))
+            max_path_length = len(max(current_paths, key=len))
             if max_path_length > tier:
                 tier = max_path_length
 
         # decrement tier to count from 0
         tier = tier - 1
-        # check if tier exists in the dict, if so add to it, else create entry
-        if tier in path_tier_dict:
-            path_tier_dict[tier].append(n)
-        else:
-            path_tier_dict[tier] = [n]
+        path_tier_dict[tier].add(n)
 
-    return {k: set(v) for k, v in path_tier_dict.items()}
+    return dict(path_tier_dict)
 
 
-def get_all_downstream_nodes(graph: Graph, node: str) -> Set[str]:
+def get_all_downstream_nodes(graph: nx.DiGraph, node: str) -> Set[str]:
     """
     Get all nodes downstream of this one (i.e. they have a path from this node to them)
 
@@ -232,9 +197,8 @@ def get_all_downstream_nodes(graph: Graph, node: str) -> Set[str]:
         A list of downstream node names
     """
     downstream_paths = []
-    diGraph = graph.directed_graph
-    for n in diGraph.nodes():
-        all_paths = [x for x in nx.all_simple_paths(diGraph, node, n)]
+    for n in graph.nodes():
+        all_paths = [x for x in nx.all_simple_paths(graph, node, n)]
         if all_paths:
             for path in all_paths:
                 downstream_paths.extend(path)
@@ -246,7 +210,7 @@ def get_all_downstream_nodes(graph: Graph, node: str) -> Set[str]:
     return downstream_nodes
 
 
-def get_successors(graph: Graph, node: str) -> Set[str]:
+def get_successors(graph: nx.DiGraph, node: str) -> Set[str]:
     """
     Get all successor nodes to the current node
 
@@ -257,11 +221,10 @@ def get_successors(graph: Graph, node: str) -> Set[str]:
     Returns:
         A set of nodes that are successors to the current node
     """
-    di_graph = graph.directed_graph
-    return set(x for x in di_graph.successors(node))
+    return set(x for x in graph.successors(node))
 
 
-def get_predecessors(graph: Graph, node: str) -> Set[str]:
+def get_predecessors(graph: nx.DiGraph, node: str) -> Set[str]:
     """
     Get all predecessor nodes to the current node
 
@@ -272,22 +235,4 @@ def get_predecessors(graph: Graph, node: str) -> Set[str]:
     Returns:
         A set of nodes that are predecessors to the current node
     """
-    di_graph = graph.directed_graph
-    return set(x for x in di_graph.predecessors(node))
-
-
-def add_edges(graph: Graph, edges: Set[tuple]) -> Graph:
-    """
-    Takes an existing graph and creates a new one with the new edge
-
-    Args:
-        graph: the graph to start with
-        edges: the edges to add
-
-    Returns:
-        A copy of initial graph with new edge
-    """
-
-    old_edges = set(graph.directed_graph.edges)
-    new_edges = old_edges.union(edges)
-    return from_edges(new_edges)
+    return set(x for x in graph.predecessors(node))
